65986cc05146f0fb9cfb2b6f0bf5f043d178e647
Philip Stehlik <p@pstehlik.com>
2019-08-17 12:58:11 +0200

    upgrade fees module to substrate 2.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -59,6 +59,13 @@ pub type Balance = u128;
 /// Used for the module template in `./template.rs`
 mod template;
 
+///// Used for anchor module
+//mod anchor;
+//
+/// Fees for TXs
+mod fees;
+
+
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
 /// of data like extrinsics, allowing for them to continue syncing the network through upgrades
@@ -210,6 +217,13 @@ impl template::Trait for Runtime {
 	type Event = Event;
 }
 
+//impl anchor::Trait for Runtime {}
+
+/// Fees module implementation
+impl fees::Trait for Runtime {
+	type Event = Event;
+}
+
 construct_runtime!(
 	pub enum Runtime where
 		Block = Block,
@@ -224,6 +238,9 @@ construct_runtime!(
 		Sudo: sudo,
 		// Used for the module template in `./template.rs`
 		TemplateModule: template::{Module, Call, Storage, Event<T>},
+//		AnchorModule: anchor::{Module, Call, Storage},
+		Fees: fees::{Module, Call, Storage, Event<T>},
+
 	}
 );
 
