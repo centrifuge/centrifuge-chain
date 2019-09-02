27293330dd5bdd0a1266d413eeb7b927c327cd69
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-09-02 10:51:53 +0200

    minor

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -131,6 +131,7 @@ impl Alternative {
                     )
                 },
                 vec![
+                    // TODO define permanant bootnodes for the network
                     String::from("/ip4/172.42.0.3/tcp/30333/p2p/QmSqbcHcJh7DvKDdMYxWREtnAfqqxLiX7J2YDGiV6e5LQq"),
                     String::from("/ip4/172.42.0.2/tcp/30333/p2p/QmctF8dCW8LBr6zqVEUJHmjmqFcsxjV91tuUL7rVLg3Zd6")
                 ],
