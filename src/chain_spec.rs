615ff26a321c5e3b3432ec21a020fbf26c073159
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-09-30 18:24:03 +0200

    Remove Alice and Bob as authorities on testnets

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -33,27 +33,26 @@ pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Pu
         .public()
 }
 
-pub fn get_from_pub_str<TPublic: Public>(pubkey_hex: &str) -> AccountId {
-    primitives::sr25519::Public::from_raw(
-        byte32_from_slice(Vec::from_hex(pubkey_hex)
-            .expect("a static hex string is valid")
-            .as_slice()))
-}
-
-fn byte32_from_slice(bytes: &[u8]) -> [u8; 32] {
-    let mut array = [0; 32];
-    let bytes = &bytes[..array.len()];
-    array.copy_from_slice(bytes);
-    array
+/// Helper function to obtain key from pubkey string
+fn get_from_pubkey_hex<TPublic: Public>(pubkey_hex: &str) -> <TPublic::Pair as Pair>::Public {
+    <TPublic::Pair as Pair>::Public::from_slice(Vec::from_hex(pubkey_hex)
+        .expect("a static hex string is valid")
+        .as_slice())
 }
 
 /// Helper function to generate stash, controller and session key from seed
-pub fn get_authority_keys_from_seed(seed: &str) -> (AccountId, AccountId, GrandpaId, BabeId) {
+pub fn get_authority_keys_from_seed(seed: &str) -> (GrandpaId, BabeId) {
     (
-        get_from_seed::<AccountId>(&format!("{}//stash", seed)),
-        get_from_seed::<AccountId>(seed),
         get_from_seed::<GrandpaId>(seed),
         get_from_seed::<BabeId>(seed),
+    )
+}
+
+/// Helper function to obtain grandpa and babe keys from pubkey strings
+pub fn get_authority_keys_from_pubkey_hex(grandpa: &str, babe: &str) -> (GrandpaId, BabeId) {
+    (
+        get_from_pubkey_hex::<GrandpaId>(grandpa),
+        get_from_pubkey_hex::<BabeId>(babe),
     )
 }
 
@@ -119,12 +118,14 @@ impl Alternative {
                 || {
                     testnet_genesis(
                         vec![
-                            get_authority_keys_from_seed("Alice"),
-                            get_authority_keys_from_seed("Bob"),
+                            get_authority_keys_from_pubkey_hex("c4051f94a879bd014647993acb2d52c4059a872b6e202e70c3121212416c5842",
+                                                               "c40526b6cb4c2ab991f5065b599a7313ba98ea6995786539ca05186adb30b34c"),
+                            get_authority_keys_from_pubkey_hex("f0415b8cdfcd189c5636f3c1d0b65637b97fdd926af8132a38f963361f293b0f",
+                                                               "c40524c8d2a97e347ba3f9c75395dabcad0ef7304c4804838f20ec05ef76b32a"),
                         ],
-                        get_from_pub_str::<AccountId>("c405224448dcd4259816b09cfedbd8df0e6796b16286ea18efa2d6343da5992e"),
+                        get_from_pubkey_hex::<AccountId>("c405224448dcd4259816b09cfedbd8df0e6796b16286ea18efa2d6343da5992e"),
                         vec![
-                            get_from_pub_str::<AccountId>("c405224448dcd4259816b09cfedbd8df0e6796b16286ea18efa2d6343da5992e")
+                            get_from_pubkey_hex::<AccountId>("c405224448dcd4259816b09cfedbd8df0e6796b16286ea18efa2d6343da5992e")
                         ],
                         true,
                     )
@@ -142,24 +143,23 @@ impl Alternative {
                 || {
                     testnet_genesis(
                         vec![
-                            // TODO remove Alice and Bob here and setup proper validator accounts. Then following RPC methods needs to be called on a validator node to start validating.
-                            // curl -H 'Content-Type: application/json' --data '{ "jsonrpc":"2.0", "method":"author_insertKey", "params":["gran", "seed"],"id":1 }' localhost:9933
-                            // curl -H 'Content-Type: application/json' --data '{ "jsonrpc":"2.0", "method":"author_insertKey", "params":["babe", "seed"],"id":1 }' localhost:9933
-                            get_authority_keys_from_seed("Alice"),
-                            get_authority_keys_from_seed("Bob"),
+                            get_authority_keys_from_pubkey_hex("c4051f94a879bd014647993acb2d52c4059a872b6e202e70c3121212416c5842",
+                                                               "c40526b6cb4c2ab991f5065b599a7313ba98ea6995786539ca05186adb30b34c"),
+                            get_authority_keys_from_pubkey_hex("f0415b8cdfcd189c5636f3c1d0b65637b97fdd926af8132a38f963361f293b0f",
+                                                               "c40524c8d2a97e347ba3f9c75395dabcad0ef7304c4804838f20ec05ef76b32a"),
                         ],
-                        get_from_pub_str::<AccountId>("c4051f94a879bd014647993acb2d52c4059a872b6e202e70c3121212416c5842"),
+                        get_from_pubkey_hex::<AccountId>("c4051f94a879bd014647993acb2d52c4059a872b6e202e70c3121212416c5842"),
                         vec![
-                            get_from_pub_str::<AccountId>("c4051f94a879bd014647993acb2d52c4059a872b6e202e70c3121212416c5842"),
-                            get_from_pub_str::<AccountId>("c40526b6cb4c2ab991f5065b599a7313ba98ea6995786539ca05186adb30b34c"),
-                            get_from_pub_str::<AccountId>("f0415b8cdfcd189c5636f3c1d0b65637b97fdd926af8132a38f963361f293b0f"),
-                            get_from_pub_str::<AccountId>("c40524c8d2a97e347ba3f9c75395dabcad0ef7304c4804838f20ec05ef76b32a"),
-                            get_from_pub_str::<AccountId>("f0415a742977038943db5f619a2101d790e8a588ba33d671044a10ea332b9f7f"),
-                            get_from_pub_str::<AccountId>("f041601cc759ea533c386a0391344e82b6efb645c07a66355411cbc657aa8c66"),
-                            get_from_pub_str::<AccountId>("f04162650738ed2e19b0240419f9680ba9d3dc6b40ccf4ad8993fcbf61ca6720"),
-                            get_from_pub_str::<AccountId>("f0415b3730410e05516cbfcdc3eb2909d373dcaf205dc1889f4455d9dc0c7222"),
-                            get_from_pub_str::<AccountId>("c4052280dcd37bc6c5148307fda2ade1be9c2d555ec49f59de27c730ca43d80d"),
-                            get_from_pub_str::<AccountId>("f04157ad160c8e5c2847f74837b1c59ad6a927bd3feb517a16e12b59a4704c7a"),
+                            get_from_pubkey_hex::<AccountId>("c4051f94a879bd014647993acb2d52c4059a872b6e202e70c3121212416c5842"),
+                            get_from_pubkey_hex::<AccountId>("c40526b6cb4c2ab991f5065b599a7313ba98ea6995786539ca05186adb30b34c"),
+                            get_from_pubkey_hex::<AccountId>("f0415b8cdfcd189c5636f3c1d0b65637b97fdd926af8132a38f963361f293b0f"),
+                            get_from_pubkey_hex::<AccountId>("c40524c8d2a97e347ba3f9c75395dabcad0ef7304c4804838f20ec05ef76b32a"),
+                            get_from_pubkey_hex::<AccountId>("f0415a742977038943db5f619a2101d790e8a588ba33d671044a10ea332b9f7f"),
+                            get_from_pubkey_hex::<AccountId>("f041601cc759ea533c386a0391344e82b6efb645c07a66355411cbc657aa8c66"),
+                            get_from_pubkey_hex::<AccountId>("f04162650738ed2e19b0240419f9680ba9d3dc6b40ccf4ad8993fcbf61ca6720"),
+                            get_from_pubkey_hex::<AccountId>("f0415b3730410e05516cbfcdc3eb2909d373dcaf205dc1889f4455d9dc0c7222"),
+                            get_from_pubkey_hex::<AccountId>("c4052280dcd37bc6c5148307fda2ade1be9c2d555ec49f59de27c730ca43d80d"),
+                            get_from_pubkey_hex::<AccountId>("f04157ad160c8e5c2847f74837b1c59ad6a927bd3feb517a16e12b59a4704c7a"),
                         ],
                         true,
                     )
@@ -185,7 +185,7 @@ impl Alternative {
 }
 
 fn testnet_genesis(
-    initial_authorities: Vec<(AccountId, AccountId, GrandpaId, BabeId)>,
+    initial_authorities: Vec<(GrandpaId, BabeId)>,
     root_key: AccountId,
     endowed_accounts: Vec<AccountId>,
     _enable_println: bool,
@@ -210,13 +210,13 @@ fn testnet_genesis(
         babe: Some(BabeConfig {
             authorities: initial_authorities
                 .iter()
-                .map(|x| (x.3.clone(), 1))
+                .map(|x| (x.1.clone(), 1))
                 .collect(),
         }),
         grandpa: Some(GrandpaConfig {
             authorities: initial_authorities
                 .iter()
-                .map(|x| (x.2.clone(), 1))
+                .map(|x| (x.0.clone(), 1))
                 .collect(),
         }),
     }
