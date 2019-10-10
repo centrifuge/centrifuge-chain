00021494450fa22a54b9b5c7c1ad52c0913c983b
vedhavyas <vedhavyas.singareddi@gmail.com>
2019-10-10 14:25:54 +0200

    rewrite hash checks

--- runtime/src/proofs.rs
+++ runtime/src/proofs.rs
@@ -7,6 +7,8 @@ pub struct Proof {
     sorted_hashes: Vec<Vec<u8>>,
 }
 
+/// validates each proof and return true if all the proofs are valid
+/// else returns false
 pub fn validate_proofs(doc_root: Vec<u8>, proofs: Vec<Proof>) -> bool {
     let mut res = false;
     for proof in proofs.iter() {
@@ -16,17 +18,22 @@ pub fn validate_proofs(doc_root: Vec<u8>, proofs: Vec<Proof>) -> bool {
     res
 }
 
+/// validates a single proof and returns true if valid
+/// else false
 fn validate_proof(doc_root: &Vec<u8>, proof: &Proof) -> bool {
     let mut hash = proof.hash.clone();
     for sorted_hash in proof.sorted_hashes.iter() {
         let mut sh = sorted_hash.clone();
         if hash > sh {
+            // hash is greater than the sorted hash, so append hash to sorted hash
             sh.extend(hash);
-            hash = runtime_io::blake2_256(sh.as_slice()).to_vec()
+            hash = sh;
         } else {
+            // hash is less than sorted_hash, so append sorted hash to hash
             hash.extend(sh);
-            hash = runtime_io::blake2_256(hash.as_slice()).to_vec()
         }
+
+        hash = runtime_io::blake2_256(hash.as_slice()).to_vec()
     }
 
     hash != *doc_root
