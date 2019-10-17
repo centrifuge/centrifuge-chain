df30542c1be3d69651d8ea28d06f83323b99844c
Vimukthi Wickramasinghe <vimukthi@centrifuge.io>
2019-10-17 14:47:11 +0200

    Set nightly version explicitly

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,4 +13,4 @@ rustup target add wasm32-unknown-unknown --toolchain $RUST_TOOLCHAIN
 
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
-	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
+	cargo +$RUST_TOOLCHAIN install --git https://github.com/alexcrichton/wasm-gc --force
