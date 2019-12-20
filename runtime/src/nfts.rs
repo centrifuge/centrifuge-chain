89e928b74ab1a9981140e87e8e886119c2556f59
philipstanislaus <6912756+philipstanislaus@users.noreply.github.com>
2019-12-20 13:05:40 +0100

    Add hash type to proofs and NFTs (#137)
    * Add hash type to proofs and NFTs
    
    * Fix tests

--- runtime/src/nfts.rs
+++ runtime/src/nfts.rs
@@ -1,7 +1,8 @@
-use crate::{anchor, proofs};
+use crate::{anchor, proofs, proofs::Proof};
 use sp_std::vec::Vec;
 use support::{decl_event, decl_module, dispatch::Result, ensure, weights::SimpleDispatchInfo};
 use system::ensure_signed;
+use primitives::H256;
 
 pub trait Trait: anchor::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
@@ -24,7 +25,7 @@ decl_module! {
         /// - depends on the arguments
         /// # </weight>
         #[weight = SimpleDispatchInfo::FixedNormal(1_500_000)]
-        fn validate_mint(origin, anchor_id: T::Hash, deposit_address: [u8; 20], pfs: Vec<proofs::Proof>, static_proofs: [[u8;32];3]) -> Result {
+        fn validate_mint(origin, anchor_id: T::Hash, deposit_address: [u8; 20], pfs: Vec<Proof>, static_proofs: [H256;3]) -> Result {
             ensure_signed(origin)?;
 
             // get the anchor data from anchor ID
@@ -46,17 +47,15 @@ decl_module! {
 impl<T: Trait> Module<T> {
     /// validates the proofs again the provided doc_root.
     /// returns false if any proofs are invalid.
-    fn validate_proofs(doc_root: T::Hash, pfs: &Vec<proofs::Proof>, static_proofs: [[u8; 32]; 3]) -> bool {
-        let mut dr: [u8; 32] = Default::default();
-        dr.clone_from_slice(doc_root.as_ref());
-        proofs::validate_proofs(dr, pfs, static_proofs)
+    fn validate_proofs(doc_root: T::Hash, pfs: &Vec<Proof>, static_proofs: [H256; 3]) -> bool {
+        proofs::validate_proofs(H256::from_slice(doc_root.as_ref()), pfs, static_proofs)
     }
 
     /// returns a Keccak hash of deposit_address + hash(keccak(name+value+salt)) of each proof provided.
-    fn get_bundled_hash(pfs: Vec<proofs::Proof>, deposit_address: [u8; 20]) -> T::Hash {
+    fn get_bundled_hash(pfs: Vec<Proof>, deposit_address: [u8; 20]) -> T::Hash {
         let bh = proofs::bundled_hash(pfs, deposit_address);
         let mut res: T::Hash = Default::default();
-        res.as_mut().copy_from_slice(&bh);
+        res.as_mut().copy_from_slice(&bh[..]);
         res
     }
 }
@@ -171,104 +170,102 @@ mod tests {
         t.into()
     }
 
-    fn get_invalid_proof() -> (Proof, H256, [[u8; 32]; 3]) {
+    fn get_invalid_proof() -> (Proof, H256, [H256; 3]) {
         let proof = Proof::new(
             [
                 1, 93, 41, 93, 124, 185, 25, 20, 141, 93, 101, 68, 16, 11, 142, 219, 3, 124, 155,
                 37, 85, 23, 189, 20, 48, 97, 34, 3, 169, 157, 88, 159,
-            ],
+            ].into(),
             vec![
                 [
                     113, 229, 58, 22, 178, 220, 200, 69, 191, 246, 171, 254, 8, 183, 211, 75, 54,
                     22, 224, 197, 170, 112, 248, 56, 10, 176, 17, 205, 86, 130, 233, 16,
-                ],
+                ].into(),
                 [
                     133, 11, 212, 75, 212, 65, 247, 178, 200, 157, 5, 39, 57, 135, 63, 126, 166,
                     92, 23, 170, 4, 155, 223, 237, 50, 237, 43, 101, 180, 104, 126, 84,
-                ],
+                ].into(),
             ],
         );
 
-        let doc_root = [
+        let doc_root: H256 = [
             48, 123, 58, 192, 8, 62, 20, 55, 99, 52, 37, 73, 174, 123, 214, 104, 37, 41, 189, 170,
             205, 80, 158, 136, 224, 128, 128, 89, 55, 240, 32, 234,
-        ]
-            .into();
+        ].into();
 
-        let static_proofs = [
+        let static_proofs: [H256;3] = [
             [
                 25, 102, 189, 46, 86, 242, 48, 217, 254, 16, 20, 211, 98, 206, 125, 92, 167, 175, 70,
                 161, 35, 135, 33, 80, 225, 247, 4, 240, 138, 86, 167, 142,
-            ],
+            ].into(),
             [
                 61, 164, 199, 22, 164, 251, 58, 14, 67, 56, 242, 60, 86, 203, 128, 203, 138, 129, 237,
                 7, 29, 7, 39, 58, 250, 42, 14, 53, 241, 108, 187, 74,
-            ],
+            ].into(),
             [
                 70, 124, 133, 120, 103, 45, 94, 174, 176, 18, 151, 243, 104, 120, 12, 54, 217, 189,
                 59, 222, 109, 64, 136, 203, 56, 136, 159,115, 96, 101, 2, 185,
-            ]
+            ].into(),
         ];
 
 
         (proof, doc_root, static_proofs)
     }
 
-    fn get_valid_proof() -> (Proof, primitives::H256, [[u8; 32]; 3]) {
+    fn get_valid_proof() -> (Proof, primitives::H256, [H256; 3]) {
         let proof = Proof::new(
             [
                 1, 93, 41, 93, 124, 185, 25, 20, 141, 93, 101, 68, 16, 11, 142, 219, 3, 124, 155,
                 37, 85, 23, 189, 209, 48, 97, 34, 3, 169, 157, 88, 159,
-            ],
+            ].into(),
             vec![
                 [
                     113, 229, 58, 223, 178, 220, 200, 69, 191, 246, 171, 254, 8, 183, 211, 75, 54,
                     223, 224, 197, 170, 112, 248, 56, 10, 176, 17, 205, 86, 130, 233, 16,
-                ],
+                ].into(),
                 [
                     133, 11, 212, 75, 212, 65, 247, 178, 200, 157, 5, 39, 57, 135, 63, 126, 166,
                     92, 232, 170, 46, 155, 223, 237, 50, 237, 43, 101, 180, 104, 126, 84,
-                ],
+                ].into(),
                 [
                     197, 248, 165, 165, 247, 119, 114, 231, 95, 114, 94, 16, 66, 142, 230, 184, 78,
                     203, 73, 104, 24, 82, 134, 154, 180, 129, 71, 223, 72, 31, 230, 15,
-                ],
+                ].into(),
                 [
                     50, 5, 28, 219, 118, 141, 222, 221, 133, 174, 178, 212, 71, 94, 64, 44, 80,
                     218, 29, 92, 77, 40, 241, 16, 126, 48, 119, 31, 6, 147, 224, 5,
-                ],
+                ].into(),
             ],
         );
 
-        let doc_root = [
+        let doc_root: H256 = [
             48, 123, 58, 192, 8, 62, 20, 55, 99, 52, 37, 73, 174, 123, 214, 104, 37, 41, 189, 170,
             205, 80, 158, 136, 224, 128, 128, 89, 55, 240, 32, 234,
-        ]
-        .into();
+        ].into();
 
-        let static_proofs = [
+        let static_proofs: [H256;3] = [
             [
                 25, 102, 189, 46, 86, 242, 48, 217, 254, 16, 20, 211, 98, 206, 125, 92, 167, 175, 70,
                 161, 35, 135, 33, 80, 225, 247, 4, 240, 138, 86, 167, 142,
-            ],
+            ].into(),
             [
                 61, 164, 199, 22, 164, 251, 58, 14, 67, 56, 242, 60, 86, 203, 128, 203, 138, 129, 237,
                 7, 29, 7, 39, 58, 250, 42, 14, 53, 241, 108, 187, 74,
-            ],
+            ].into(),
             [
                 70, 124, 133, 120, 103, 45, 94, 174, 176, 18, 151, 243, 104, 120, 12, 54, 217, 189,
                 59, 222, 109, 64, 136, 203, 56, 136, 159,115, 96, 101, 2, 185,
-            ]
+            ].into(),
         ];
 
         (proof, doc_root, static_proofs)
     }
 
-    fn get_params() -> (primitives::H256, [u8; 20], Vec<Proof>, [[u8; 32]; 3]) {
+    fn get_params() -> (primitives::H256, [u8; 20], Vec<Proof>, [H256; 3]) {
         let anchor_id = <Test as system::Trait>::Hashing::hash_of(&0);
         let deposit_address: [u8; 20] = [0; 20];
         let pfs: Vec<Proof> = vec![];
-        let static_proofs: [[u8; 32]; 3] = [[0;32], [0;32], [0;32]];
+        let static_proofs: [H256; 3] = [[0;32].into(), [0;32].into(), [0;32].into()];
         (anchor_id, deposit_address, pfs, static_proofs)
     }
 
