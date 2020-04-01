c54d416472cdf45fe71f72d683c7f14546c08116
Jovansonlee Cesar <ivanceras@gmail.com>
2020-04-02 01:01:50 +0800

    change the hardcoded base_commit in add_impl_version, sub_impl_version to the BASE_COMMIT variable
    Add color formatting RED for fail, GREEN for OK and yellow for the labels, so it is easy to see what we are looking for when it auto-fails

--- ci/check_pr.sh
+++ ci/check_pr.sh
@@ -8,6 +8,15 @@
 
 # get the commit sha for this PR
 CI_COMMIT_SHA=$(git rev-parse HEAD)
+echo "commit: ${CI_COMMIT_SHA}"
+
+LABEL_BREAKS_API="breaks-api"
+LABEL_CHANGES_RUNTIME="changes-runtime"
+red='\033[0;31m'
+bold='\033[1m'
+nc='\033[0m' # No Color
+green='\033[01;32m'
+yellow='\033[01;33m'
 
 
 boldprint () { printf "|\n| \033[1m${@}\033[0m\n|\n" ; }
@@ -40,7 +49,7 @@ RUNTIME_FILE_CHANGED=$(echo "${CHANGED_FILES}" | grep -e ^runtime/ | wc -l)
 # The labels is then extracted from the PR info using
 #   jq '.labels' | jq ' .[] | .name '
 #
-# ie: curl -s https://api.github.com/repos/centrifuge/centrifuge-chain/pulls/119 | jq '.labels' | jq '.[] | .name'
+# ie: curl -s https://api.github.com/repos/centrifuge/centrifuge-chain/pulls/119 | jq '.labels' | jq -r '.[] | .name'
 # This will list down the list of labels on that PR
 
 
@@ -62,26 +71,29 @@ github_label_from_pr () {
 	pr_info=$(curl -s https://api.github.com/repos/centrifuge/centrifuge-chain/pulls/${1})
 	labels=$(echo "${pr_info}" | jq '.labels' )
 	if [ "$labels" != "null" ]; then 
-		label_names=$(echo "${labels}" | jq '.[] | .name')
+		label_names=$(echo "${labels}" | jq -r '.[] | .name')
 		echo "${label_names}"
 	fi
 }
 
 
 PR_ID=$(github_pr_from_commit "${CI_COMMIT_SHA}")
-pr_label=$(github_label_from_pr "${PR_ID}")
+PR_LABEL=$(github_label_from_pr "${PR_ID}")
 
-echo "PR labels: ${pr_label}"
 
-LABEL_MARKER="breakapi"
+echo -e ""
+echo -e "| PR labels: ${yellow}${PR_LABEL}${nc}"
 
-LABELED_MARKER_COUNT=$(echo -e "${pr_label}" | grep -w ${LABEL_MARKER} | wc -l)
+
+LABEL_BREAK_API_COUNT=$(echo "${PR_LABEL}" | grep -w ${LABEL_BREAKS_API} | wc -l)
+LABEL_CHANGES_RUNTIME_COUNT=$(echo "${PR_LABEL}" | grep -w ${LABEL_CHANGES_RUNTIME} | wc -l)
+
 
 if [ $RUNTIME_FILE_CHANGED != "0" ]
 then
 	echo "There are ${RUNTIME_FILE_CHANGED} file(s) changed in runtime "
 
-	if [ "${LABELED_MARKER_COUNT}" != "0" ]
+	if [ "${LABEL_BREAK_API_COUNT}" != "0" ]
 	then
 		add_spec_version="$(git diff ${BASE_COMMIT}...${CI_COMMIT_SHA} ${VERSIONS_FILE} \
 			| sed -n -r "s/^\+[[:space:]]+spec_version: +([0-9]+),$/\1/p")"
@@ -90,26 +102,35 @@ then
 
 		if [ "${add_spec_version}" != "${sub_spec_version}" ]
 		then
-			echo "OK: spec_version is changed.. "
+			echo -e "| ${green}${bold}OK:${nc} PR has label ${yellow}${LABEL_BREAKS_API}${nc} and spec_version is changed.. "
 			exit 0
 		else
-			echo "ERROR: spec_version should be changed in ${VERSIONS_FILE}"
+			echo -e "| ${red}${bold}ERROR:${nc} PR has label ${yellow}${LABEL_BREAKS_API}${nc}, but spec_version remains the same in ${VERSIONS_FILE}"
 		fi
-	else
-		echo "Not a breaking change"
+	elif [ "${LABEL_CHANGES_RUNTIME_COUNT}" != "0" ]
+	then
 
-		add_impl_version="$(git diff tags/release...${CI_COMMIT_SHA} ${VERSIONS_FILE} \
+		add_impl_version="$(git diff ${BASE_COMMIT}...${CI_COMMIT_SHA} ${VERSIONS_FILE} \
 			| sed -n -r 's/^\+[[:space:]]+impl_version: +([0-9]+),$/\1/p')"
-		sub_impl_version="$(git diff tags/release...${CI_COMMIT_SHA} ${VERSIONS_FILE} \
+		sub_impl_version="$(git diff ${BASE_COMMIT}...${CI_COMMIT_SHA} ${VERSIONS_FILE} \
 			| sed -n -r 's/^\-[[:space:]]+impl_version: +([0-9]+),$/\1/p')"
 
 		if [ "${add_impl_version}" != "${sub_impl_version}" ]
 		then
-			echo "OK: impl_version is changed..."
+			echo -e "| ${green}${bold}OK:${nc} PR has label ${yellow}${LABEL_CHANGES_RUNTIME}${nc} and impl_version is changed..."
 			exit 0
 		else
-			echo "ERROR: impl_version should be changed in ${VERSIONS_FILE}"
+			echo -e "| ${red}${bold}ERROR:${nc} PR has label ${yellow}${LABEL_CHANGES_RUNTIME}${nc}, but impl_version remains the same in ${VERSIONS_FILE}"
 		fi
+	else
+		echo -e ""
+		echo -e "| ${red}${bold}ERROR:${nc} There are changes in runtime but PR has no required label"
+		echo -e "|    Required label is one of: ${yellow}${LABEL_BREAKS_API}${nc},${yellow}${LABEL_CHANGES_RUNTIME}${nc}"
+		if [ "${PR_LABEL}" != "" ]
+		then
+			echo -e "| PR has these labels: ${yellow}${PR_LABEL}${nc}"
+		fi
+		echo -e "| "
 	fi
 else
 	echo "OK: No changes in runtime, no need for version change"
