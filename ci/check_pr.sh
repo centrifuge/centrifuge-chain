aaef97a79676538dc5323625b06fdbbcfc034050
Jovansonlee Cesar <ivanceras@gmail.com>
2020-04-02 23:10:41 +0800

    Improve the script, add more comments and make it explicit on the exit fail

--- ci/check_pr.sh
+++ ci/check_pr.sh
@@ -4,13 +4,13 @@
 # - if runtime files are changed:
 #	- Check if the spec_version is incremented
 # 	- If spec_version incremented, ensure impl_version is 0 and exit successfully
-# 	- if spec_version is not incremented, check if the impl_version is incremented
+# 	- if spec_version is NOT incremented, check if the impl_version is incremented
 # 		- if impl_version incremented, then exit successfully
-# 		- if not then fail the script
-# - if runtime files are not changed, exit successfully
+# 		- if impl_version is NOT incremented then fail the script
+# - if runtime files are NOT changed, exit successfully
 #
 
-# base the origin/master as the base commit
+# origin/master as the base commit
 BASE_COMMIT="origin/master"
 VERSIONS_FILE="runtime/src/lib.rs"
 
@@ -26,12 +26,13 @@ yellow='\033[01;33m'
 
 OK="${green}${block}OK${nc}"
 ERROR="${red}${block}ERROR${nc}"
+FATAL="${red}${block}FATAL${nc}"
 
 
 # show the diff of origin/master and this PR sha
 CHANGED_FILES=$(git diff --name-only ${BASE_COMMIT}...${PR_COMMIT})
 
-echo "Changed files: ${CHANGED_FILES}"
+#echo "Changed files: ${CHANGED_FILES}"
 
 # count the number of files changed in runtime directory
 RUNTIME_FILE_CHANGED=$(echo "${CHANGED_FILES}" | grep -e ^runtime/ | wc -l)
@@ -45,17 +46,28 @@ then
 	exit 0
 fi
 
-
+# Extract the spec_version  and impl_version from origin/master and the PR
 BASE_SPEC_VERSION=$(git show ${BASE_COMMIT}:${VERSIONS_FILE} | sed -n -r "s/^[[:space:]]+spec_version: +([0-9]+),$/\1/p")
 BASE_IMPL_VERSION=$(git show ${BASE_COMMIT}:${VERSIONS_FILE} | sed -n -r "s/^[[:space:]]+impl_version: +([0-9]+),$/\1/p")
 
 PR_SPEC_VERSION=$(git show ${PR_COMMIT}:${VERSIONS_FILE} | sed -n -r "s/^[[:space:]]+spec_version: +([0-9]+),$/\1/p")
 PR_IMPL_VERSION=$(git show ${PR_COMMIT}:${VERSIONS_FILE} | sed -n -r "s/^[[:space:]]+impl_version: +([0-9]+),$/\1/p")
 
+# Show the changes in the versions
+echo "|"
 echo "| ${BASE_COMMIT} spec_version: ${BASE_SPEC_VERSION}"
 echo "| ${BASE_COMMIT} impl_version: ${BASE_IMPL_VERSION}"
+echo "| "
 echo "| PR spec_version: ${PR_SPEC_VERSION}"
 echo "| PR impl_version: ${PR_IMPL_VERSION}"
+echo "|"
+
+# spec_version should never decremented, show a fatal alert it that's the case
+if (( $PR_SPEC_VERSION < $BASE_SPEC_VERSION ))
+then
+	echo -e "${FATAL}: ${yellow}spec_version${nc} is decremented"
+	exit 1
+fi
 
 # Check if the PR spec version is incremented
 if (( $PR_SPEC_VERSION > $BASE_SPEC_VERSION ))
@@ -67,8 +79,11 @@ then
 		echo -e "| ${OK}: ${yellow}impl_version${nc} is set to 0"
 		exit 0
 	else
+		# The impl_version is not reset to 0 and should fail
 		echo -e "| ${ERROR}: ${yellow}impl_version${nc} must be reset to 0 when ${yellow}spec_version${nc} is incremented"
+		exit 1
 	fi
+# The spec_version is not changed
 else
 	# if spec_version is not incremented
 	# Check if impl_version is incremented
@@ -78,8 +93,10 @@ else
 		echo -e "| ${OK}: ${yellow}impl_version${nc} is incremented"
 		exit 0
 	else
+		# The impl_version is not incremented, and should fail
 		echo -e "| ${ERROR}: ${yellow}impl_version${nc} is NOT incremented"
-		echo -e "|	Note: either ${yellow}impl_version${nc} or ${yellow}spec_version${nc} should be incremented when there is changed in the runtime"
+		echo -e "|	Note: either ${yellow}impl_version${nc} or ${yellow}spec_version${nc} should be incremented when there is changes in the runtime"
+		exit 1
 	fi
 fi
 
