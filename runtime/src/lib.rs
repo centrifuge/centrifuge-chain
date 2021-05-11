fdf288c270f2c166895308b79899f876cfc5528c
Branan Riley <branan@centrifuge.io>
2021-05-11 09:22:46 -0700

    Bump runtime version
    * `transaction_version` goes up since the pallets are different in
    parachain vs. standalone
    * `spec_version` is just set to a sufficiently large value that we
    won't conflict with any changes to the standalone chain

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -127,10 +127,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // and set impl_version to 0. If only runtime
     // implementation changes and behavior does not, then leave spec_version as
     // is and increment impl_version.
-    spec_version: 240,
+    spec_version: 1000,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 1,
+    transaction_version: 2,
 };
 
 /// Native version.
