d3b3023c046f27f5bfdf994d173108110a70f443
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-05-17 19:59:07 +0200

    review changs

--- ci/script.sh
+++ ci/script.sh
@@ -37,5 +37,5 @@ case $TARGET in
 
   build-runtime)
     export RUSTC_VERSION=$RUST_TOOLCHAIN
-    docker run --rm -e RUNTIME_DIR=./runtime -v $PWD:/build -v $TMPDIR/cargo:/cargo-home chevdor/srtool:$RUSTC_VERSION build
+    docker run --rm -e RUNTIME_DIR=./runtime -v $PWD:/build -v /tmp/cargo:/cargo-home chevdor/srtool:$RUSTC_VERSION build
 esac
