1bb2ff227b5203a38f442fd027c70bea21397aab
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-05-19 09:23:03 +0200

    ignore codde cov

--- ci/script.sh
+++ ci/script.sh
@@ -21,4 +21,8 @@ case $TARGET in
   build-runtime)
     export RUSTC_VERSION=$RUST_TOOLCHAIN
     docker run --rm -e RUNTIME_DIR=./runtime -e PACKAGE=centrifuge-chain-runtime -v $PWD:/build -v /tmp/cargo:/cargo-home chevdor/srtool:$RUSTC_VERSION build
+    ;;
+
+  tests)
+    cargo test -p bridge-mapping --release
 esac
