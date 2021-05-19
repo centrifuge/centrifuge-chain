47ad89b03ff227aa4256dd29f29fb728a6e5d5b8
Binadamu ISIYOONEKANA <isiyoonekana@gmail.com>
2021-05-19 14:19:56 +0200

    Port pallet to FRAME v2, using new pallet attribute macros

---
+++ pallets/rad-claims/README.md
@@ -0,0 +1,90 @@
+# Rad Claims Pallet
+
+<!-- TOC -->
+
+- [Rad Claims Pallet](#rad-claims-pallet)
+    - [Overview](#overview)
+    - [Pallet Usage](#pallet-usage)
+        - [Add the Pallet to your Parachain Runtime](#add-the-pallet-to-your-parachain-runtime)
+        - [Configure the Pallet](#configure-the-pallet)
+    - [Pallet Documentation](#pallet-documentation)
+    - [References](#references)
+    - [License](#license)
+
+<!-- /TOC -->
+
+## Overview
+
+This [FRAME](https://substrate.dev/docs/en/knowledgebase/runtime/frame) pallet 
+provides functionalities for processing RAD token rewarding claims.
+
+## Pallet Usage
+
+### Add the Pallet to your Parachain Runtime
+
+In order to add this pallet to your runtime, you should add the following lines
+to your parachain's main `Cargo.toml` file:
+
+```toml
+# -- snip --
+
+[dependencies.pallet-rad-claims]            # <-- Add the new dependency
+default_features = false
+git = 'https://github.com/centrifuge-chain/pallet-rad-claims.git'
+branch = master
+
+# -- snip --
+
+[features]
+std = [
+    # -- snip --
+    'pallet-rad-claims/std',                # <-- Add this line
+]
+```
+
+### Configure the Pallet
+
+Now that the pallet is added to your runtime,  the latter must be configured
+for your runtime (in `[runtime_path]/lib.rs` file):
+
+```rust
+
+// Parameterize Rad claims pallet
+ord_parameter_types! {
+    pub const RadClaimsPalletId: ModuleId = PalletId(*b"rd/claim");
+    pub const One: u64 = 1;
+    pub const Longevity: u32 = 64;
+    pub const UnsignedPriority: TransactionPriority = TransactionPriority::max_value();
+    pub cosnt MinimalPayoutAmount: u128 = node_primitives::Balance = 5 * currency::RAD;
+}
+
+// Implement Rad claims pallet configuration trait for the mock runtime
+impl pallet_rad_claims::Config for MyRuntime {
+    type Event = ();
+    type PalletId = RadClaimsPalletId;
+    type Longevity = Longevity;
+    type UnsignedPriority = UnsignedPriority;
+    type AdminOrigin = EnsureSignedBy<One, u64>;
+    type Currency = Balances;
+}
+
+construct_runtime! {
+    …
+
+    RadClaims: pallet_rad_claims::{Module, Call, Config<T>, Storage, Event<T>, ValidateUnsigned},
+}
+```
+
+## Pallet Documentation
+
+You can see this pallet's reference documentation with the following command:
+
+```sh
+$ cargo doc --package rad-claims --open
+```
+
+The table of contents for this markdown file is automatically generated using the [`auto-markdown-toc`](https://marketplace.visualstudio.com/items?itemName=huntertran.auto-markdown-toc) extension for Visual StudioCode.
+
+## References
+
+## License
---
+++ pallets/rad-claims/src/mock.rs
@@ -0,0 +1,231 @@
+// Copyright 2021 Parity Technologies (UK) Ltd.
+// This file is part of Centrifuge (centrifuge.io) parachain.
+
+// Cumulus is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Cumulus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
+
+
+//! Rad claims pallet testing environment and utilities
+//!
+//! The main components implemented in this mock module is a mock runtime
+//! and some helper functions.
+
+
+#![cfg(test)]
+
+
+// ----------------------------------------------------------------------------
+// Imports and dependencies
+// ----------------------------------------------------------------------------
+
+use crate::{
+    self as pallet_rad_claims,
+    Config
+};
+
+use frame_support::{
+    impl_outer_origin, 
+    ord_parameter_types, 
+    PalletId, 
+    parameter_types, 
+    weights::Weight
+};
+
+use frame_system::EnsureSignedBy;
+
+use sp_core::H256;
+use sp_io::TestExternalities;
+
+use sp_runtime::{
+    testing::Header,
+    traits::{
+        BlakeTwo256, 
+        IdentityLookup
+    }, 
+    transaction_validity::{
+        TransactionPriority, 
+    },
+};
+
+use crate::traits::WeightInfo;
+
+pub use pallet_balances as balances;
+use pallet_balances::Error as BalancesError;
+use centrifuge_runtime::constants::currency;
+
+
+// ----------------------------------------------------------------------------
+// Types and constants declaration
+// ----------------------------------------------------------------------------
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<MockRuntime>;
+type Block = frame_system::mocking::MockBlock<MockRuntime>;
+
+// Implement testint extrinsic weights for the pallet
+pub struct MockWeightInfo;
+impl WeightInfo for MockWeightInfo {
+
+    fn claim() -> Weight { 
+        0 as Weight 
+    }
+
+    fn set_upload_account() -> Weight { 
+        0 as Weight 
+    }
+
+    fn store_root_hash() -> Weight { 
+        0 as Weight 
+    }
+}
+
+pub const ADMIN: u64 = 0x1;
+pub const USER_A: u64 = 0x2;
+
+// USER_B does not have existential balance
+pub const USER_B: u64 = 0x3;
+
+pub const ENDOWED_BALANCE: u128 = 10000 * currency::RAD;
+
+
+// ----------------------------------------------------------------------------
+// Mock runtime configuration
+// ----------------------------------------------------------------------------
+
+// Build mock runtime
+frame_support::construct_runtime!(
+    pub enum MockRuntime where 
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+        Balances: pallet_balances::{Pallet, Call, Config<T>, Storage, Event<T>},
+        RadClaims: pallet_rad_claims::{Pallet, Call, Config<T>, Storage, Event<T>, ValidateUnsigned},
+    }
+);
+
+// Parameterize FRAME system pallet
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+}
+
+// Implement FRAME system pallet configuration trait for the mock runtime
+impl frame_system::Config for MockRuntime {
+    type AccountId = u64;
+    type Call = Call;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Index = u64;
+    type BlockNumber = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type Header = Header;
+    type Event = Event;
+    type Origin = Origin;
+    type BlockHashCount = BlockHashCount;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = balances::AccountData<u128>;
+    type OnNewAccount = ();
+    type OnKilledAccount = balances::Pallet<MockRuntime>;
+    type DbWeight = ();
+    type BaseCallFilter = ();
+    type SystemWeightInfo = ();
+}
+
+// Parameterize FRAME balances pallet
+parameter_types! {
+    pub const ExistentialDeposit: u64 = 1;
+}
+
+// Implement FRAME balances pallet configuration trait for the mock runtime
+impl pallet_balances::Config for MockRuntime {
+    type Balance = u128;
+    type DustRemoval = ();
+    type Event = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type WeightInfo = ();
+}
+
+// Parameterize RAD claims pallet
+parameter_types! {
+    pub const RadClaimsPalletId: PalletId = PalletId(*b"rd/claim");
+    pub const One: u64 = 1;
+    pub const Longevity: u32 = 64;
+    pub const UnsignedPriority: TransactionPriority = TransactionPriority::max_value();
+    pub const MinimalPayoutAmount: u128 = node_primitives::Balance = 5 * currency::RAD;
+}
+
+// Implement RAD claims pallet configuration trait for the mock runtime
+impl Config for MockRuntime {
+    type Event = Event;
+    type PalletId = RadClaimsPalletId;
+    type Longevity = Longevity;
+    type UnsignedPriority = UnsignedPriority;
+    type AdminOrigin = EnsureSignedBy<One, u64>;
+    type Currency = Balances;
+}
+
+
+// ----------------------------------------------------------------------------
+// Test externalities
+// ----------------------------------------------------------------------------
+
+// Test externalities builder type declaraction.
+//
+// This type is mainly used for mocking storage in tests. It is the type alias 
+// for an in-memory, hashmap-based externalities implementation.
+pub struct TestExternalitiesBuilder {
+    existential_deposit: u64
+}
+
+// Default trait implementation for test externalities builder
+impl Default for TestExternalitiesBuilder {
+	fn default() -> Self {
+		Self {
+            existential_deposit: 1
+		}
+	}
+}
+
+impl TestExternalitiesBuilder {
+    
+    // Set FRAME balances pallet's existential deposit
+    pub fn existential_deposit(mut self, existential_deposit: u64) -> Self {
+        self.existential_deposit = existential_deposit;
+        self
+    }
+    
+    // Build a genesis storage key/value store
+	pub(crate) fn build(self) -> TestExternalities {
+		let storage = frame_system::GenesisConfig::default()
+            .build_storage::<MockRuntime>()
+            .unwrap();
+
+        // pre-fill balances
+        pallet_balances::GenesisConfig::<MockRuntime> {
+            balances: vec![
+                (ADMIN, ENDOWED_BALANCE), 
+                (USER_A, 1), 
+                (RadClaims::account_id(), ENDOWED_BALANCE)
+            ],
+        }.assimilate_storage(&mut storage).unwrap();
+        
+        let mut externalities = TestExternalities::new(storage);
+
+        externalities.execute_with(|| System::set_block_number(1));
+	    externalities
+    }
+}
