81e29c57937ab7ecb5eec0316733b949e82e82c3
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2021-06-02 12:14:34 -0700

    Adding Rumba and Charcol Specs

---
+++ scripts/export_parachain_files.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+chain_name=$1
+parachain_id=$2
+should_build=$3
+
+if [[ $chain_name == "" || $parachain_id == "" ]]; then
+  echo "Chain Name or Parachain ID argument not provided"
+  exit 1
+fi
+
+FILE=./target/release/centrifuge-chain
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist, rebuild binary to continue"
+    exit 1
+fi
+
+if [[ $should_build == "true" ]]; then
+  echo "Building Spec for $chain_name"
+  $PWD/target/release/centrifuge-chain build-spec --chain $chain_name --disable-default-bootnode > res/$chain_name-spec.json
+  $PWD/target/release/centrifuge-chain build-spec --chain res/$chain_name-spec.json --disable-default-bootnode --raw > res/$chain_name-spec-raw.json
+fi
+
+echo "Exporting State & Wasm"
+$PWD/target/release/centrifuge-chain export-genesis-state --chain res/$chain_name-spec-raw.json --parachain-id $parachain_id > $chain_name-genesis-state
+$PWD/target/release/centrifuge-chain export-genesis-wasm --chain res/$chain_name-spec-raw.json > $chain_name-genesis-wasm
--- src/command.rs
+++ src/command.rs
@@ -39,11 +39,13 @@ fn load_spec(
 	para_id: ParaId,
 ) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
 	match id {
+		"cyclone" | "" => Ok(Box::new(chain_spec::cyclone_config())),
+		"altair" => Ok(Box::new(chain_spec::altair_config())),
+		"charcoal-local" => Ok(Box::new(chain_spec::charcoal_local_network(para_id))),
 		"charcoal-rococo" => Ok(Box::new(chain_spec::charcoal_rococo_config())),
 		"charcoal-rococo-staging" => Ok(Box::new(chain_spec::charcoal_rococo_staging_network(para_id))),
-		"charcoal-chachacha-local" => Ok(Box::new(chain_spec::charcoal_local_network(para_id))),
-		"charcoal-chachacha-staging" => Ok(Box::new(chain_spec::charcoal_chachacha_staging_network(para_id))),
-		"charcoal-chachacha" => Ok(Box::new(chain_spec::charcoal_chachacha_config())),
+		"rumba-chachacha-staging" => Ok(Box::new(chain_spec::rumba_chachacha_staging_network(para_id))),
+		"rumba-chachacha" => Ok(Box::new(chain_spec::rumba_chachacha_config())),
 		path => Ok(Box::new(chain_spec::ChainSpec::from_json_file(
 			path.into(),
 		)?)),
@@ -52,7 +54,7 @@ fn load_spec(
 
 impl SubstrateCli for Cli {
 	fn impl_name() -> String {
-		"Centrifuge Charcoal Parachain Collator".into()
+		"Centrifuge Parachain Collator".into()
 	}
 
 	fn impl_version() -> String {
