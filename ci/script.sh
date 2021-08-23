252d847d6ef7ca675a34c4dc06f3b39d1f61961d
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2021-08-23 16:05:52 +0200

    Use srtool supported 1.53.0 for wasm builds (#459)
    * Use srtool supported 1.53.0 on all builds
    
    * Use srtool supported 1.53.0 on all builds
    
    * attempt fix build 1
    
    * hardcode altair runtime - for now

--- ci/script.sh
+++ ci/script.sh
@@ -3,6 +3,7 @@
 set -eux
 
 RUST_TOOLCHAIN="${RUST_TOOLCHAIN:-nightly-2021-07-28}"
+PACKAGE="${PACKAGE:-altair-runtime}" #Need to replicate job for all runtimes
 
 # Enable warnings about unused extern crates
 export RUSTFLAGS=" -W unused-extern-crates"
@@ -20,7 +21,7 @@ case $TARGET in
 
   build-runtime)
     export RUSTC_VERSION=$RUST_TOOLCHAIN
-    docker run --rm -e RUNTIME_DIR=./runtime -e PACKAGE=centrifuge-chain-runtime -v $PWD:/build -v /tmp/cargo:/cargo-home chevdor/srtool:$RUSTC_VERSION build
+    docker run --rm -e PACKAGE=$PACKAGE -v $PWD:/build -v /tmp/cargo:/cargo-home chevdor/srtool:$RUSTC_VERSION build
     ;;
 
   tests)
