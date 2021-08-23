e98523026fbd05dd598c509050a3861a19fc5704
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2021-08-23 14:48:54 +0200

    hardcode altair runtime - for now

--- ci/script.sh
+++ ci/script.sh
@@ -3,8 +3,7 @@
 set -eux
 
 RUST_TOOLCHAIN="${RUST_TOOLCHAIN:-nightly-2021-07-28}"
-RUNTIME_DIR="${RUNTIME_DIR:-./runtime}"
-PACKAGE="${PACKAGE:-centrifuge-chain-runtime}"
+PACKAGE="${PACKAGE:-altair-runtime}" #Need to replicate job for all runtimes
 
 # Enable warnings about unused extern crates
 export RUSTFLAGS=" -W unused-extern-crates"
@@ -22,7 +21,7 @@ case $TARGET in
 
   build-runtime)
     export RUSTC_VERSION=$RUST_TOOLCHAIN
-    docker run --rm -e RUNTIME_DIR=$RUNTIME_DIR -e PACKAGE=$PACKAGE -v $PWD:/build -v /tmp/cargo:/cargo-home chevdor/srtool:$RUSTC_VERSION build
+    docker run --rm -e PACKAGE=$PACKAGE -v $PWD:/build -v /tmp/cargo:/cargo-home chevdor/srtool:$RUSTC_VERSION build
     ;;
 
   tests)
