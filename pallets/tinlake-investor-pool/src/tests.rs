30afa5af4dd4763790b2eb3d2558238ce3b30efd
Branan Riley <branan@centrifuge.io>
2021-10-21 15:54:46 -0700

    Epoch execution testing

--- pallets/tinlake-investor-pool/src/tests.rs
+++ pallets/tinlake-investor-pool/src/tests.rs
@@ -37,10 +37,11 @@ fn core_constraints_currency_available_cant_cover_redemptions() {
 			max_reserve: 40,
 			available_reserve: Zero::zero(),
 			total_reserve: 39,
+			fake_nav: 0,
 		};
 
 		let epoch = EpochExecutionInfo {
-			nav: Zero::zero(),
+			nav: 0,
 			reserve: pool.total_reserve,
 			tranches: epoch_tranches,
 		};
@@ -84,7 +85,7 @@ fn pool_constraints_pool_reserve_above_max_reserve() {
 		let tranches = vec![tranche_a, tranche_b, tranche_c, tranche_d];
 		let epoch_tranches = tranches
 			.iter()
-			.zip(vec![80, 20, 5, 5]) // no IntoIterator for arrays, so we use a vec here. Meh.
+			.zip(vec![80, 20, 15, 15]) // no IntoIterator for arrays, so we use a vec here. Meh.
 			.map(|(tranche, value)| EpochExecutionTranche {
 				value,
 				price: One::one(),
@@ -104,10 +105,11 @@ fn pool_constraints_pool_reserve_above_max_reserve() {
 			max_reserve: 5,
 			available_reserve: Zero::zero(),
 			total_reserve: 40,
+			fake_nav: 90,
 		};
 
 		let epoch = EpochExecutionInfo {
-			nav: Zero::zero(),
+			nav: 90,
 			reserve: pool.total_reserve,
 			tranches: epoch_tranches,
 		};
@@ -176,10 +178,11 @@ fn pool_constraints_tranche_violates_sub_ratio() {
 			max_reserve: 150,
 			available_reserve: Zero::zero(),
 			total_reserve: 50,
+			fake_nav: 0,
 		};
 
 		let epoch = EpochExecutionInfo {
-			nav: Zero::zero(),
+			nav: 0,
 			reserve: pool.total_reserve,
 			tranches: epoch_tranches,
 		};
@@ -228,7 +231,7 @@ fn pool_constraints_pass() {
 
 		let epoch_tranches = tranches
 			.iter()
-			.zip(vec![80, 20, 5, 5]) // no IntoIterator for arrays, so we use a vec here. Meh.
+			.zip(vec![80, 70, 35, 20]) // no IntoIterator for arrays, so we use a vec here. Meh.
 			.map(|(tranche, value)| EpochExecutionTranche {
 				value,
 				price: One::one(),
@@ -248,10 +251,11 @@ fn pool_constraints_pass() {
 			max_reserve: 150,
 			available_reserve: Zero::zero(),
 			total_reserve: 50,
+			fake_nav: 145,
 		};
 
 		let epoch = EpochExecutionInfo {
-			nav: Zero::zero(),
+			nav: 145,
 			reserve: pool.total_reserve,
 			tranches: epoch_tranches,
 		};
@@ -267,5 +271,116 @@ fn pool_constraints_pass() {
 			&epoch,
 			&full_solution
 		));
+	});
+}
+
+#[test]
+fn epoch() {
+	new_test_ext().execute_with(|| {
+		let tin_investor = Origin::signed(0);
+		let drop_investor = Origin::signed(1);
+		let pool_owner = Origin::signed(2);
+		let borrower = Origin::signed(3);
+		let pool_account = Origin::signed(PoolLocator { pool_id: 0 }.into_account());
+
+		// Initialize pool with initial investments
+		assert_ok!(TinlakeInvestorPool::create_pool(
+			pool_owner.clone(),
+			0,
+			vec![(10, 10), (0, 0)],
+			CurrencyId::Usd,
+			10_000 * CURRENCY
+		));
+		assert_ok!(TinlakeInvestorPool::order_supply(
+			tin_investor.clone(),
+			0,
+			1,
+			500 * CURRENCY
+		));
+		assert_ok!(TinlakeInvestorPool::order_supply(
+			drop_investor.clone(),
+			0,
+			0,
+			500 * CURRENCY
+		));
+		assert_ok!(TinlakeInvestorPool::close_epoch(pool_owner.clone(), 0));
+		assert_ok!(Tokens::transfer(
+			pool_account.clone(),
+			0,
+			CurrencyId::Tranche(0, 1),
+			500 * CURRENCY
+		));
+		assert_ok!(Tokens::transfer(
+			pool_account.clone(),
+			1,
+			CurrencyId::Tranche(0, 0),
+			500 * CURRENCY
+		));
+
+		let pool = TinlakeInvestorPool::pool(0).unwrap();
+		assert_eq!(pool.tranches[0].debt, 0);
+		assert_eq!(pool.tranches[0].reserve, 500 * CURRENCY);
+		assert_eq!(pool.tranches[0].ratio, Perquintill::from_float(0.5));
+		assert_eq!(pool.tranches[1].debt, 0);
+		assert_eq!(pool.tranches[1].reserve, 500 * CURRENCY);
+		assert_eq!(pool.available_reserve, 1000 * CURRENCY);
+		assert_eq!(pool.total_reserve, 1000 * CURRENCY);
+
+		// Borrow some money
+		next_block();
+		assert_ok!(TinlakeInvestorPool::test_borrow(
+			borrower.clone(),
+			0,
+			500 * CURRENCY
+		));
+
+		let pool = TinlakeInvestorPool::pool(0).unwrap();
+		assert_eq!(pool.tranches[0].debt, 250 * CURRENCY);
+		assert_eq!(pool.tranches[0].reserve, 250 * CURRENCY);
+		assert_eq!(pool.tranches[1].debt, 250 * CURRENCY);
+		assert_eq!(pool.tranches[1].reserve, 250 * CURRENCY);
+		assert_eq!(pool.available_reserve, 500 * CURRENCY);
+		assert_eq!(pool.total_reserve, 500 * CURRENCY);
+
+		// Repay (with made up interest) after a month.
+		next_block_after(60 * 60 * 24 * 30);
+		assert_ok!(TinlakeInvestorPool::test_nav_up(
+			borrower.clone(),
+			0,
+			10 * CURRENCY
+		));
+		assert_ok!(TinlakeInvestorPool::test_payback(
+			borrower.clone(),
+			0,
+			510 * CURRENCY
+		));
+
+		let pool = TinlakeInvestorPool::pool(0).unwrap();
+		assert_eq!(pool.tranches[0].debt, 0);
+		assert!(pool.tranches[0].reserve > 500 * CURRENCY); // there's interest in here now
+		assert_eq!(pool.tranches[1].debt, 0);
+		assert_eq!(pool.tranches[1].reserve, 500 * CURRENCY); // not yet rebalanced
+		assert_eq!(pool.available_reserve, 500 * CURRENCY);
+		assert_eq!(pool.total_reserve, 1010 * CURRENCY);
+
+		// DROP investor tries to redeem
+		next_block();
+		assert_ok!(TinlakeInvestorPool::order_redeem(
+			drop_investor.clone(),
+			0,
+			0,
+			250 * CURRENCY
+		));
+		assert_ok!(TinlakeInvestorPool::close_epoch(pool_owner.clone(), 0));
+
+		let pool = TinlakeInvestorPool::pool(0).unwrap();
+		// assert_eq!(pool.tranches[0].epoch_redeem, 0);
+		assert_eq!(pool.tranches[0].debt, 0);
+		assert!(pool.tranches[0].reserve > 250 * CURRENCY); // there's interest in here now
+		assert_eq!(pool.tranches[1].debt, 0);
+		assert!(pool.tranches[1].reserve > 500 * CURRENCY); // not yet rebalanced
+		assert_eq!(pool.available_reserve, pool.total_reserve);
+		assert!(pool.total_reserve > 750 * CURRENCY);
+		assert!(pool.total_reserve < 800 * CURRENCY);
 	});
 }
