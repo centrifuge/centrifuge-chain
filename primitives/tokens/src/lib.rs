391ffc0980ca2edc8110d9d721a152922d8432b7
mustermeiszer <mustermeiszer@posteo.de>
2022-01-17 21:06:05 +0100

    Fix mock of pool pallet, use restricted-tokens there

--- primitives/tokens/src/lib.rs
+++ primitives/tokens/src/lib.rs
@@ -9,6 +9,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Debug, Encode, Decode, TypeInfo)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub enum CurrencyId {
+	Native,
 	Usd,
 	Tranche(u64, u8),
 }
