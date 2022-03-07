7024454a924465bdf797d6f4c49b1ee3223e6ff8
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2022-03-07 11:43:51 -0800

    test catalyst onboarding

--- runtime/centrifuge/src/lib.rs
+++ runtime/centrifuge/src/lib.rs
@@ -65,7 +65,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("centrifuge"),
 	impl_name: create_runtime_str!("centrifuge"),
 	authoring_version: 1,
-	spec_version: 1001,
+	spec_version: 1000,
 	impl_version: 1,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
