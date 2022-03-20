8de6d69d650d57c8c61a4788f7b36b9d2b58798e
Jeroen Offerijns <jeroen@offerijns.com>
2022-03-20 12:32:51 +0100

    feat: set up interest rates pallet

--- libs/common-traits/src/lib.rs
+++ libs/common-traits/src/lib.rs
@@ -126,6 +126,18 @@ pub trait PoolReserve<AccountId>: PoolInspect<AccountId> {
 	fn deposit(pool_id: Self::PoolId, from: AccountId, amount: Self::Balance) -> DispatchResult;
 }
 
+/// A trait that can be used to calculate interest accrual for debt
+pub trait InterestRates<InterestRate, Amount> {
+	type NormalizedDebt;
+	type Adjustment;
+
+	/// Calculate the current debt using normalized debt * cumulative rate
+	fn get_current_debt(interest_rate_per_sec: InterestRate, normalized_debt: Self::NormalizedDebt) -> Result<Amount, DispatchError>;
+	
+	/// Increase or decrease the normalized debt
+	fn adjust_normalized_debt(interest_rate_per_sec: InterestRate, normalized_debt: Self::NormalizedDebt, adjustment: Self::Adjustment) -> Result<Amount, DispatchError>;
+}
+
 pub trait Permissions<AccountId> {
 	type Location;
 	type Role;
