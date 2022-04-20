a5141810cc5c6e01045ee3397151cedb3e86cb68
nuno <hi@nunoalexandre.com>
2022-04-20 12:44:14 +0200

    Document `parachains`

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -235,6 +235,13 @@ pub mod constants {
 	}
 }
 
+/// Listing of parachains we integrate with.
+/// For each parachain, we are interested in stating their parachain ID
+/// as well as any of their token key ID that we possibly support in our
+/// XCM configuration. These token keys are defined in said parachain
+/// and must always match the value there defined, which is expected to
+/// never change once defined since they help define the canonical id
+/// of said tokens in the network, which is relevant for XCM transfers.
 pub mod parachains {
 	pub mod karura {
 		pub const ID: u32 = 2000;
