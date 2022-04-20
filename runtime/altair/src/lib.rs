6bd556e334b7e49b225b519d012edf6b2e1f3057
nuno <hi@nunoalexandre.com>
2022-04-20 12:46:03 +0200

    Bump altair version

--- runtime/altair/src/lib.rs
+++ runtime/altair/src/lib.rs
@@ -83,7 +83,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("altair"),
 	impl_name: create_runtime_str!("altair"),
 	authoring_version: 1,
-	spec_version: 1011,
+	spec_version: 1012,
 	impl_version: 1,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
