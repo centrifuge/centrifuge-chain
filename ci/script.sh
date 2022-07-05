97f6ee328f5b0d1f2b08a010401672cbc80dea7b
Nuno Alexandre <hi@nunoalexandre.com>
2022-07-05 23:52:35 +0200

    ci: Add SRTOOL_VERSION to differentiate from RUSTC (#858)

--- ci/script.sh
+++ ci/script.sh
@@ -3,7 +3,8 @@
 set -eux
 
 RUST_TOOLCHAIN="${RUST_TOOLCHAIN:-nightly-2022-05-09}"
-PACKAGE="${PACKAGE:-centrifuge-runtime}" #Need to replicate job for all runtimes
+SRTOOL_VERSION="${SRTOOL_VERSION:-1.60.0}"
+PACKAGE="${PACKAGE:-centrifuge-runtime}" # Need to replicate job for all runtimes
 
 # Enable warnings about unused extern crates
 export RUSTFLAGS=" -W unused-extern-crates"
@@ -21,7 +22,7 @@ case $TARGET in
 
   build-runtime)
     export RUSTC_VERSION=$RUST_TOOLCHAIN
-    docker run --rm -e PACKAGE=$PACKAGE -v $PWD:/build -v /tmp/cargo:/cargo-home paritytech/srtool:$RUSTC_VERSION build
+    docker run --rm -e PACKAGE=$PACKAGE -v $PWD:/build -v /tmp/cargo:/cargo-home paritytech/srtool:$SRTOOL_VERSION build
     ;;
 
   tests)
