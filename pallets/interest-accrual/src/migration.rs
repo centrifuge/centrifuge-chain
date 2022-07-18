c0c9a8402dad8a01737036a8bf5d8a9dadfe3458
Branan Riley <branan@centrifuge.io>
2022-07-18 08:59:57 -0700

    Add interest-accrual migration file

---
+++ pallets/interest-accrual/src/migration.rs
@@ -0,0 +1,28 @@
+use crate::*;
+use frame_support::{traits::Get, weights::Weight};
+
+pub mod v1 {
+	use super::*;
+	pub fn migrate<T: Config>() -> Weight {
+		let mut weight = T::DbWeight::get().reads_writes(1, 1);
+		let now = Pallet::<T>::now();
+		LastUpdated::<T>::set(now);
+		Rate::<T>::translate(|per_sec, rate: RateDetailsV0Of<T>| {
+			let accumulated_rate = Pallet::<T>::calculate_accumulated_rate(
+				per_sec,
+				rate.accumulated_rate,
+				rate.last_updated,
+			)
+			.expect("Our values are too small to overflow");
+			let delta = now - rate.last_updated;
+			let _bits = Moment::BITS - delta.leading_zeros();
+			weight += T::DbWeight::get().reads_writes(1, 1);
+			// weight += T::Weight::calculate_accumulated_rate(_bits);
+			Some(RateDetailsOf::<T> {
+				accumulated_rate,
+				reference_count: 0,
+			})
+		});
+		weight
+	}
+}
