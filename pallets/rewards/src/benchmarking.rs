6cccc18c23ff03f2daad8b7f2f88f54d12308d03
lemunozm <lemunozm@gmail.com>
2022-09-01 22:19:26 +0200

    prepared template for pallet-rewards

---
+++ pallets/rewards/src/benchmarking.rs
@@ -0,0 +1,18 @@
+use super::*;
+
+#[allow(unused)]
+use crate::Pallet as Rewards;
+use frame_benchmarking::{benchmarks, whitelisted_caller};
+use frame_system::RawOrigin;
+
+benchmarks! {
+	do_something {
+		let s in 0 .. 100;
+		let caller: T::AccountId = whitelisted_caller();
+	}: _(RawOrigin::Signed(caller), s)
+	verify {
+		assert_eq!(Something::<T>::get(), Some(s));
+	}
+
+	impl_benchmark_test_suite!(Rewards, crate::mock::new_test_ext(), crate::mock::Test);
+}
