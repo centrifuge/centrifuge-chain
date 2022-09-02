81e7164fd622ab95c522f5478b6ab4ff7080eaa2
Bastian Gruber <foreach@me.com>
2022-09-02 10:11:36 +0200

    [WIP] pallet-anchors: add precommit deposit (#879)
    * Rename internal variable
    
    * Add deposit for precommit
    
    * Update mocks and runtimes to support anchor deposit changes
    
    * unreserve by the original account id
    
    * store the deposit value when precommit to evict with the same value
    
    * Add deposit testing
    
    * Refactor precommit eviction. Evict precommit on commit
    
    * Adapt precommit related tests
    
    * Update pallet doc
    
    * Removing the unused errors
    
    * Add benchmark
    
    * add precommit benchmark
    
    * add commit and evict_pre_commits benchmarks
    
    * add evict_anchors benchmarks
    
    * Add benchmark support for runtime
    
    * fix anchor pallet name for benchmarks
    
    * Remove wrong check
    
    * Fix nix cargo sha

--- pallets/anchors/src/mock.rs
+++ pallets/anchors/src/mock.rs
@@ -10,6 +10,7 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
+
 use crate::{self as pallet_anchors, Config};
 
 use common_traits::{fees::test_util::MockFees, impl_mock_fees_state};
