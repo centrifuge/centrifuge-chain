593e9c3092520df4d61fa2a91ae2c7c59ddcbf2b
Bastian Gruber <foreach@me.com>
2022-09-02 09:46:53 +0200

    cargo fmt

--- runtime/integration-tests/src/pools/utils/mod.rs
+++ runtime/integration-tests/src/pools/utils/mod.rs
@@ -9,7 +9,6 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
 pub mod accounts;
 pub mod env;
 pub mod extrinsics;
