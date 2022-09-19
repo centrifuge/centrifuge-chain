29d05daa2bf0f7affde01b822f0f547565a58cd5
Branan Riley <branan@centrifuge.io>
2022-09-19 10:58:25 -0700

    Fix clippy::redundant-static-lifetimes

--- libs/primitives/src/lib.rs
+++ libs/primitives/src/lib.rs
@@ -223,7 +223,7 @@ pub mod constants {
 
 	// Represents the protobuf encoding - "NFTS". All Centrifuge documents are formatted in this way.
 	/// These are pre/appended to the registry id before being set as a [RegistryInfo] field in [create_registry].
-	pub const NFTS_PREFIX: &'static [u8] = &[1, 0, 0, 0, 0, 0, 0, 20];
+	pub const NFTS_PREFIX: &[u8] = &[1, 0, 0, 0, 0, 0, 0, 20];
 
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
 		items as Balance * 15 * CENTI_CFG + (bytes as Balance) * 6 * CENTI_CFG
