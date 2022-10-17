6a2553c0676fc02518186bf5f0baef0673387ce2
Bastian Gruber <foreach@me.com>
2022-10-17 14:57:24 +0200

    flake.nix + cargo fmt

--- flake.nix
+++ flake.nix
@@ -93,7 +93,7 @@
             };
 
             # This is a hash of all the Cargo dependencies, for reproducibility.
-            cargoSha256 = "9ZuqCQeyuShXB/h9iwEAh1/waZ2nDSJIbAEVUS78Shk=";
+            cargoSha256 = "e+ee5kakCmndvKqvjlD39HpILv3IGTeeC4Sy8wN/PHU=";
 
             nativeBuildInputs = with pkgs; [ clang git-mock pkg-config ];
             buildInputs = with pkgs; [ openssl ] ++ (
