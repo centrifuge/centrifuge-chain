d753fb14981839e7388f3a71f825992d11c00bcf
Bastian Gruber <foreach@me.com>
2022-10-20 09:07:15 +0200

    Fix command.rs

--- src/command.rs
+++ src/command.rs
@@ -536,10 +536,10 @@ impl CliConfiguration<Self> for RelayChainCli {
 	}
 
 	fn base_path(&self) -> Result<Option<BasePath>> {
-		Ok(self
+		self
 			.shared_params()
 			.base_path()
-			.or_else(|| self.base_path.clone().map(Into::into)))
+			.or_else(|_| Ok(self.base_path.clone().map(Into::into)))
 	}
 
 	fn role(&self, is_dev: bool) -> Result<sc_service::Role> {
@@ -548,10 +548,6 @@ impl CliConfiguration<Self> for RelayChainCli {
 
 	fn transaction_pool(&self, is_dev: bool) -> Result<sc_service::config::TransactionPoolOptions> {
 		self.base.base.transaction_pool(is_dev)
-	}
-
-	fn state_cache_child_ratio(&self) -> Result<Option<usize>> {
-		self.base.base.state_cache_child_ratio()
 	}
 
 	fn chain_id(&self, is_dev: bool) -> Result<String> {
