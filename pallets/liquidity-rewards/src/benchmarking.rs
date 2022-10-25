d8a557f6cf23377056eebbe49506d64a8115907a
lemunozm <lemunozm@gmail.com>
2022-10-25 12:30:21 +0200

    factorize default reward mock for benchmarking

--- pallets/liquidity-rewards/src/benchmarking.rs
+++ pallets/liquidity-rewards/src/benchmarking.rs
@@ -12,6 +12,40 @@ const GROUP_B: u32 = 101;
 const WEIGHT: u32 = 23;
 const CURRENCY_ID_A: u32 = 42;
 
+fn init_test_mock_with_expectations() -> impl Sized {
+	#![allow(unused_variables)]
+	let mock = ();
+
+	// Pallet mock config uses MockRewards that is expected to be configured with some context expectation.
+	// We configure it we some sensible/default values for benchmarks.
+	#[cfg(test)]
+	let mock = {
+		let lock = cfg_traits::rewards::mock::lock();
+
+		let ctx1 = MockRewards::group_stake_context();
+		ctx1.expect().return_const(100u64);
+
+		let ctx2 = MockRewards::reward_group_context();
+		ctx2.expect().return_const(Ok(()));
+
+		let ctx3 = MockRewards::deposit_stake_context();
+		ctx3.expect().return_const(Ok(()));
+
+		let ctx4 = MockRewards::withdraw_stake_context();
+		ctx4.expect().return_const(Ok(()));
+
+		let ctx5 = MockRewards::claim_reward_context();
+		ctx5.expect().return_const(Ok(0));
+
+		let ctx6 = MockRewards::attach_currency_context();
+		ctx6.expect().return_const(Ok(()));
+
+		(lock, ctx1, ctx2, ctx3, ctx4, ctx5, ctx6)
+	};
+
+	mock
+}
+
 benchmarks! {
 	where_clause {
 		where
@@ -27,21 +61,7 @@ benchmarks! {
 		let y in 0..T::MaxChangesPerEpoch::get(); // currency changes
 		let z in 0..T::MaxChangesPerEpoch::get(); // weight changes
 
-		#[cfg(test)]
-		let mock = {
-			let m = cfg_traits::rewards::mock::lock();
-
-			let ctx1 = MockRewards::group_stake_context();
-			ctx1.expect().return_const(100u64);
-
-			let ctx2 = MockRewards::reward_group_context();
-			ctx2.expect().return_const(Ok(()));
-
-			let ctx3 = MockRewards::attach_currency_context();
-			ctx3.expect().return_const(Ok(()));
-
-			(m, ctx1, ctx2, ctx3)
-		};
+		let mock = init_test_mock_with_expectations();
 
 		for i in 0..x {
 			// Specify weights to have set groups when perform the last on_initialize.
@@ -68,94 +88,46 @@ benchmarks! {
 		Pallet::<T>::on_initialize(frame_system::Pallet::<T>::block_number());
 	}
 	verify {
-		#[cfg(test)]
-		{
-			mock.1.checkpoint();
-			mock.2.checkpoint();
-			mock.3.checkpoint();
-		}
+		drop(mock);
 	}
 
 	stake {
 		let caller = whitelisted_caller();
 
-		#[cfg(test)]
-		let mock = {
-			let ctx1 = MockRewards::deposit_stake_context();
-			ctx1.expect().return_const(Ok(()));
-
-			let ctx2 = MockRewards::attach_currency_context();
-			ctx2.expect().return_const(Ok(()));
-
-			(ctx1, ctx2)
-		};
+		let mock = init_test_mock_with_expectations();
 
 		Pallet::<T>::set_currency_group(RawOrigin::Root.into(), CURRENCY_ID_A.into(), GROUP_A.into()).unwrap();
 		Pallet::<T>::on_initialize(T::BlockNumber::zero());
 
 	}: _(RawOrigin::Signed(caller), CURRENCY_ID_A.into(), T::Balance::zero())
 	verify {
-		#[cfg(test)]
-		{
-			mock.0.checkpoint();
-			mock.1.checkpoint();
-		}
+		drop(mock);
 	}
 
 	unstake {
 		let caller = whitelisted_caller();
 
-		#[cfg(test)]
-		let mock = {
-			let m = cfg_traits::rewards::mock::lock();
-
-			let ctx1 = MockRewards::withdraw_stake_context();
-			ctx1.expect().return_const(Ok(()));
-
-			let ctx2 = MockRewards::attach_currency_context();
-			ctx2.expect().return_const(Ok(()));
-
-			(m, ctx1, ctx2)
-		};
+		let mock = init_test_mock_with_expectations();
 
 		Pallet::<T>::set_currency_group(RawOrigin::Root.into(), CURRENCY_ID_A.into(), GROUP_A.into()).unwrap();
 		Pallet::<T>::on_initialize(T::BlockNumber::zero());
 
 	}: _(RawOrigin::Signed(caller), CURRENCY_ID_A.into(), T::Balance::zero())
 	verify {
-		#[cfg(test)]
-		{
-			mock.1.checkpoint();
-			mock.2.checkpoint();
-		}
+		drop(mock);
 	}
 
 	claim_reward {
 		let caller = whitelisted_caller();
 
-		#[cfg(test)]
-		let mock = {
-			let m = cfg_traits::rewards::mock::lock();
-
-			let ctx1 = MockRewards::claim_reward_context();
-			ctx1.expect().return_const(Ok(0));
-
-			let ctx2 = MockRewards::attach_currency_context();
-			ctx2.expect().return_const(Ok(()));
-
-			(m, ctx1, ctx2)
-		};
+		let mock = init_test_mock_with_expectations();
 
 		Pallet::<T>::set_currency_group(RawOrigin::Root.into(), CURRENCY_ID_A.into(), GROUP_A.into()).unwrap();
 		Pallet::<T>::on_initialize(T::BlockNumber::one());
 
 	}: _(RawOrigin::Signed(caller), CURRENCY_ID_A.into())
 	verify {
-		#[cfg(test)]
-		{
-			mock.1.checkpoint();
-			mock.2.checkpoint();
-		}
+		drop(mock);
 	}
 
 	set_distributed_reward {
