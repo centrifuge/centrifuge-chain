64daf4868c4c55ccc2179e475fbba508c3cc7848
Bastian Gruber <foreach@me.com>
2022-10-27 08:48:49 -0400

    Update pallets/pool-registry/src/tests.rs
    Co-authored-by: Jeroen Offerijns <Offerijns@users.noreply.github.com>

--- pallets/pool-registry/src/tests.rs
+++ pallets/pool-registry/src/tests.rs
@@ -34,7 +34,7 @@ fn set_metadata() {
 			let pool_owner = 9u64;
 			let pool_id = 0;
 
-			assert_ok!(PoolsRegistry::set_metadata(
+			assert_ok!(PoolRegistry::set_metadata(
 				Origin::signed(pool_owner),
 				pool_id,
 				"QmUTwA6RTUb1FbJCeM1D4G4JaMHAbPehK6WwCfykJixjm3" // random IPFS hash, for test purposes
