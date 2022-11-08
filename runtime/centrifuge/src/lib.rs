ecf472e72a2e9b877f649a6680ffb24dd8580f8a
nuno <hi@nunoalexandre.com>
2022-11-08 08:37:57 +0100

    bump: Centrifuge 1015 + Altair 2023

--- runtime/centrifuge/src/lib.rs
+++ runtime/centrifuge/src/lib.rs
@@ -91,7 +91,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("centrifuge"),
 	impl_name: create_runtime_str!("centrifuge"),
 	authoring_version: 1,
-	spec_version: 1014,
+	spec_version: 1015,
 	impl_version: 1,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
