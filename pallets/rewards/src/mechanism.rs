7bf4438c2711cc4750a79f2f8367552067b77c6b
lemunozm <lemunozm@gmail.com>
2022-11-14 02:55:08 +0100

    add generic distribution_id type instead of a hardcoded distribution_count type

--- pallets/rewards/src/mechanism.rs
+++ pallets/rewards/src/mechanism.rs
@@ -1,3 +1,4 @@
+use cfg_traits::ops::ensure::EnsureAddAssign;
 use frame_support::traits::tokens::Balance;
 use sp_runtime::{traits::Get, ArithmeticError};
 
@@ -6,15 +7,47 @@ pub mod base_with_currency_movement;
 
 pub mod deferred;
 
+pub trait DistributionId: Sized {
+	fn next_id(&mut self) -> Result<Self, ArithmeticError>;
+}
+
+impl DistributionId for () {
+	fn next_id(&mut self) -> Result<Self, ArithmeticError> {
+		Ok(())
+	}
+}
+
+macro_rules! distribution_id_impl {
+	($number:ty) => {
+		impl DistributionId for $number {
+			fn next_id(&mut self) -> Result<Self, ArithmeticError> {
+				self.ensure_add_assign(1)?;
+				Ok(*self)
+			}
+		}
+	};
+}
+
+distribution_id_impl!(u8);
+distribution_id_impl!(u16);
+distribution_id_impl!(u32);
+distribution_id_impl!(u64);
+distribution_id_impl!(u128);
+
 pub trait RewardMechanism {
 	type Group;
 	type Account;
 	type Currency;
 	type Balance: Balance;
+	type DistributionId: DistributionId;
 	type MaxCurrencyMovements: Get<u32>;
 
 	/// Reward the group mutating the group entity.
-	fn reward_group(group: &mut Self::Group, amount: Self::Balance) -> Result<(), ArithmeticError>;
+	fn reward_group(
+		group: &mut Self::Group,
+		amount: Self::Balance,
+		distribution_id: Self::DistributionId,
+	) -> Result<(), ArithmeticError>;
 
 	/// Add stake to the account and mutates currency and group to archieve that.
 	fn deposit_stake(
@@ -83,7 +116,8 @@ pub mod test {
 		(
         $mechanism:ident,
         $initial:ident,
-        $expectation:ident
+        $expectation:ident,
+        $distribution_id:expr
         ) => {
 			#[test]
 			fn reward_group() {
@@ -91,7 +125,8 @@ pub mod test {
 
 				frame_support::assert_ok!($mechanism::reward_group(
 					&mut group,
-					crate::mechanism::test::REWARD
+					crate::mechanism::test::REWARD,
+					$distribution_id
 				));
 
 				assert_eq!(group, *$expectation::REWARD_GROUP__GROUP);
