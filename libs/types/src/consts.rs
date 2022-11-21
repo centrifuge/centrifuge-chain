30d542fe1b29ba35d10a5294e1c1221fbdf16c19
Bastian Gruber <foreach@me.com>
2022-11-21 14:29:00 +0100

    Fix typo

--- libs/types/src/consts.rs
+++ libs/types/src/consts.rs
@@ -7,7 +7,7 @@
 // (at your option) any later version (see http://www.gnu.org/licenses).
 // Centrifuge is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See t
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // Pools-related constants
