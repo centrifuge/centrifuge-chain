3991c5562e35932f1576cc23f89cf55549d764e4
Thea Leake <thea@centrifuge.io>
2022-12-06 11:56:39 +0100

    0.9.32 upgrade config runtime changes for liquidity-rewards pallet

--- pallets/liquidity-rewards/src/lib.rs
+++ pallets/liquidity-rewards/src/lib.rs
@@ -151,10 +151,10 @@ pub mod pallet {
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
 		/// Required origin for admin purposes for configuring groups and currencies.
-		type AdminOrigin: EnsureOrigin<Self::Origin>;
+		type AdminOrigin: EnsureOrigin<Self::RuntimeOrigin>;
 
 		/// Type used to handle balances.
 		type Balance: Balance + MaxEncodedLen + FixedPointOperand;
