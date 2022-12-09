2a420108b12d9088d7c5757dbb4e4a5d48c2650c
Thea Leake <thea@centrifuge.io>
2022-12-09 14:20:57 +0100

    0.9.32 upgrade removed old {pre|post}_migrate scheduler fns from dev runtime
    https://github.com/paritytech/substrate/pull/11649/files#diff-402ebe8b386bd8422131875c431704f41f17f30dd1cebe04b96082b561ef899b

--- runtime/development/src/lib.rs
+++ runtime/development/src/lib.rs
@@ -1749,16 +1749,6 @@ impl frame_support::traits::OnRuntimeUpgrade for SchedulerMigrationV3 {
 	fn on_runtime_upgrade() -> frame_support::weights::Weight {
 		Scheduler::migrate_v2_to_v4()
 	}
-
-	#[cfg(feature = "try-runtime")]
-	fn pre_upgrade() -> Result<(), &'static str> {
-		Scheduler::pre_migrate_to_v3()
-	}
-
-	#[cfg(feature = "try-runtime")]
-	fn post_upgrade() -> Result<(), &'static str> {
-		Scheduler::post_migrate_to_v3()
-	}
 }
 
 #[cfg(not(feature = "disable-runtime-api"))]
