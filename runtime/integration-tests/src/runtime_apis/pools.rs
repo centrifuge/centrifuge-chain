13eb9983d45f1ba2cceccc95de8caa70ed14eb9a
Bastian Gruber <foreach@me.com>
2023-01-18 17:18:26 +0100

    Adjust tests

--- runtime/integration-tests/src/runtime_apis/pools.rs
+++ runtime/integration-tests/src/runtime_apis/pools.rs
@@ -19,6 +19,10 @@ use fudge::primitives::Chain;
 use sp_core::Pair;
 use sp_runtime::{app_crypto::sr25519, traits::IdentifyAccount};
 use tokio::runtime::Handle;
+use cfg_primitives::{AccountId, Moment};
+use cfg_types::permissions::{PermissionScope, PoolRole, Role};
+use cfg_utils::set_block_number_timestamp;
+use pallet_loans::math;
 
 use super::{ApiEnv, PARA_ID};
 use crate::{
@@ -27,7 +31,7 @@ use crate::{
 	pools::utils::{
 		accounts::Keyring,
 		env::{test_env_default, TestEnv},
-		loans::{init_loans_for_pool, issue_default_loan, LoanId, NftManager},
+		loans::{init_loans_for_pool, issue_default_loan, LoanId, NftManager, borrow_call},
 		pools::default_pool_calls,
 	},
 };
@@ -36,12 +40,15 @@ use crate::{
 async fn test() {
 	ApiEnv::new(Handle::current())
 		.startup(|| {
+			let pool_admin: AccountId = Keyring::Alice.into();
+			let borrower: AccountId = Keyring::Bob.into();
+
 			let mut nft_manager = NftManager::new();
-			let set_default_pool = default_pool_calls(Keyring::Admin.into(), 0, &mut nft_manager);
+			let set_default_pool = default_pool_calls(pool_admin.clone(), 0, &mut nft_manager);
 			let set_loans_for_pools =
-				init_loans_for_pool(Keyring::Admin.into(), 0, &mut nft_manager);
+				init_loans_for_pool(pool_admin.clone(), 0, &mut nft_manager);
 			let issue_default_loans = issue_default_loan(
-				Keyring::Admin.into(),
+				pool_admin.clone(),
 				0,
 				10_000 * 10_000_000_000,
 				90 * 60 * 60 * 24,
@@ -51,25 +58,37 @@ async fn test() {
 			for call in set_default_pool {
 				let res = UnfilteredDispatchable::dispatch_bypass_filter(
 					call,
-					RuntimeOrigin::signed(Keyring::Admin.into()),
+					RuntimeOrigin::signed(pool_admin.clone()),
 				);
 				assert_ok!(res);
 			}
 			for call in set_loans_for_pools {
 				let res = UnfilteredDispatchable::dispatch_bypass_filter(
 					call,
-					RuntimeOrigin::signed(Keyring::Admin.into()),
+					RuntimeOrigin::signed(pool_admin.clone()),
 				);
 			}
 			for call in issue_default_loans {
 				let res = UnfilteredDispatchable::dispatch_bypass_filter(
 					call,
-					RuntimeOrigin::signed(Keyring::Admin.into()),
+					RuntimeOrigin::signed(pool_admin.clone()),
 				);
 				assert_ok!(res);
 			}
 
-			development_runtime::Timestamp::set_timestamp(86400000 * 3);
+			// set timestamp to around 1 year
+			let now = development_runtime::Timestamp::now();
+			let after_one_year = now + 31_536_000 * 1000;
+			set_block_number_timestamp::<Runtime>(Default::default(), after_one_year.into());
+
+			let borrow_call = UnfilteredDispatchable::dispatch_bypass_filter(
+				borrow_call(0, LoanId::from(0_u16), 100_000),
+				RuntimeOrigin::signed(pool_admin.clone()),
+			);
+
+			assert_ok!(borrow_call);
+			// let max_borrow_amount = development_runtime::Loans::get_max_borrow_amount(0, LoanId::from(0_u16));
+			// assert_eq!(max_borrow_amount, Ok(0));
 		})
 		.with_api(|api, latest| {
 			let valuation = api.portfolio_valuation(&latest, 0).unwrap();
@@ -78,6 +97,6 @@ async fn test() {
 			let max_borrow_amount = api
 				.max_borrow_amount(&latest, 0, LoanId::from(0_u16))
 				.unwrap();
-			assert_eq!(max_borrow_amount, None);
+			assert_eq!(max_borrow_amount, Ok(0));
 		});
 }
