38c3d7ce063ad138b51745fa3c3eae20785f67be
Bastian Gruber <foreach@me.com>
2023-01-20 14:04:59 +0100

    flake.nix

--- flake.nix
+++ flake.nix
@@ -93,7 +93,7 @@
             };
 
             # This is a hash of all the Cargo dependencies, for reproducibility.
-            cargoSha256 = "sha256-U1LaD8Tbc62waq6TCK2bN4loRvSUu9+CKkEUjB098fg=";
+            cargoSha256 = "0000000000000000000000000000000000000000000000000000";
 
             nativeBuildInputs = with pkgs; [ clang git-mock pkg-config ];
             buildInputs = with pkgs; [ openssl ] ++ (
