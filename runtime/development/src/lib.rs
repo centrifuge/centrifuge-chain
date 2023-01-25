001795fe29a2584f140cd57d54cf455ad999070d
nuno <hi@nunoalexandre.com>
2023-01-25 17:07:55 +0100

    dev: Bump spec_version to 1009

--- runtime/development/src/lib.rs
+++ runtime/development/src/lib.rs
@@ -123,7 +123,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("centrifuge-devel"),
 	impl_name: create_runtime_str!("centrifuge-devel"),
 	authoring_version: 1,
-	spec_version: 1008,
+	spec_version: 1009,
 	impl_version: 1,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
