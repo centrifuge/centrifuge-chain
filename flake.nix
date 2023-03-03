6f3ddbd5c2b2c0923164456e7bb6a3dec693ef32
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2023-03-03 17:57:32 +0100

    Add `mock-builder` to the repo with pools & permissions instances (#1220)
    * mock-builder with test passing
    
    * add pools and permissions mocks
    
    * fix codeowners file
    
    * nix hash
    
    * update nix
    
    * add comment, fix nix
    
    * taplo fmt
    
    * add example tests
    
    * test rename
    
    * add missing feature, taplo fmt, nix fix
    
    * nix hash

--- flake.nix
+++ flake.nix
@@ -93,7 +93,7 @@
             };
 
             # This is a hash of all the Cargo dependencies, for reproducibility.
-            cargoSha256 = "sha256-2+BxyO+CR3O4qjIcNj0ckEOZMxYjR2s3HNJqutMlSxw=";
+            cargoSha256 = "sha256-yJaqhK3VkTn6B6liO4LvYgRQueVk72nwNnVj96Grnog=";
 
             nativeBuildInputs = with pkgs; [ clang git-mock pkg-config ];
             buildInputs = with pkgs; [ openssl ] ++ (
