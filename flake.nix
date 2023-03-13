756e8d5000b910b02bd9fec0d4821e9601d26cc9
lemunozm <lemunozm@gmail.com>
2023-03-13 14:14:47 +0100

    fix nix

--- flake.nix
+++ flake.nix
@@ -93,7 +93,7 @@
             };
 
             # This is a hash of all the Cargo dependencies, for reproducibility.
-            cargoSha256 = "sha256-SsEMOSiSwZsk/6x9Q3n4fV07XTwHfjKrQBZl9HIlB5c=";
+            cargoSha256 = "sha256-pObzTUndn2KWxbuH1Q7g1ms6lBTcJhySh5tDsF7UuA4=";
 
             nativeBuildInputs = with pkgs; [ clang git-mock pkg-config ];
             buildInputs = with pkgs; [ openssl ] ++ (
