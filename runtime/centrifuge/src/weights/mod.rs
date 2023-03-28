319b4a9382af7dd4f1799d03dc073a68ef28464b
mustermeiszer <mustermeiszer@posteo.de>
2023-03-28 08:47:22 +0200

    feat: update weights centrifuge

--- runtime/centrifuge/src/weights/mod.rs
+++ runtime/centrifuge/src/weights/mod.rs
@@ -9,10 +9,12 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
+pub mod cumulus_pallet_xcmp_queue;
 pub mod frame_system;
 pub mod pallet_anchors;
 pub mod pallet_balances;
 pub mod pallet_collator_allowlist;
+pub mod pallet_collator_selection;
 pub mod pallet_collective;
 pub mod pallet_crowdloan_claim;
 pub mod pallet_crowdloan_reward;
@@ -29,6 +31,7 @@ pub mod pallet_pool_registry;
 pub mod pallet_pool_system;
 pub mod pallet_preimage;
 pub mod pallet_proxy;
+pub mod pallet_restricted_tokens;
 pub mod pallet_scheduler;
 pub mod pallet_timestamp;
 pub mod pallet_treasury;
