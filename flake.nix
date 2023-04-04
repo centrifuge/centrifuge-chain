9cd3270fe78fadcaebf4f0927fff71d1f2b5bf7c
mustermeiszer <mustermeiszer@posteo.de>
2023-04-04 11:56:08 +0200

    feat: bumb runtime version

--- flake.nix
+++ flake.nix
@@ -93,7 +93,7 @@
             };
 
             # This is a hash of all the Cargo dependencies, for reproducibility.
-            cargoSha256 = "sha256-V+ST911USf0FCpVxpJEwRrbwXjvLDQ0aHtNoIokH1M4=";
+            cargoSha256 = "sha256-K2DhxFEkaxhp+aAW455ALyZMuN2VI+GKwe+wuTPY/IE=";
 
             nativeBuildInputs = with pkgs; [ clang git-mock pkg-config ];
             buildInputs = with pkgs; [ openssl ] ++ (
