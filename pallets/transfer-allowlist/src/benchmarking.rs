e5b018b292d72c66dab069615dc1ed3ebdf68bf3
Thea Leake <thea@centrifuge.io>
2023-05-08 14:55:35 +0200

    Fix fmt in macros

--- pallets/transfer-allowlist/src/benchmarking.rs
+++ pallets/transfer-allowlist/src/benchmarking.rs
@@ -37,8 +37,8 @@ benchmarks! {
 	add_transfer_allowance_no_existing_metadata {
 	let (sender, receiver) = set_up_users::<T>();
 	}:add_transfer_allowance(RawOrigin::Signed(sender.clone()), CurrencyId::Native, receiver.clone().into())
-			verify {
-	  assert_eq!(
+	verify {
+		assert_eq!(
 			Pallet::<T>::get_account_currency_transfer_allowance(
 				(sender,
 				CurrencyId::Native,
@@ -88,21 +88,21 @@ benchmarks! {
 		)
 	}
 	add_allowance_delay_existing_metadata {
-			let (sender, receiver) = set_up_users::<T>();
-			Pallet::<T>::add_transfer_allowance(RawOrigin::Signed(sender.clone()).into(), CurrencyId::Native, receiver.clone().into())?;
+		let (sender, receiver) = set_up_users::<T>();
+		Pallet::<T>::add_transfer_allowance(RawOrigin::Signed(sender.clone()).into(), CurrencyId::Native, receiver.clone().into())?;
 	}:add_allowance_delay(RawOrigin::Signed(sender.clone()), CurrencyId::Native, 200u32.into())
 		verify {
 			assert_eq!(
 					Pallet::<T>::get_account_currency_restriction_count_delay(
-						  sender,
-								CurrencyId::Native,
-					).unwrap(),
-					AllowanceMetadata {
-						  allowance_count: 1,
-						  current_delay: Some(200u32.into()),
-						  once_modifiable_after: None
-					}
-			  )
+					sender,
+					CurrencyId::Native,
+				).unwrap(),
+				AllowanceMetadata {
+					allowance_count: 1,
+					current_delay: Some(200u32.into()),
+					once_modifiable_after: None
+				}
+			)
 		}
 
 
@@ -133,15 +133,15 @@ benchmarks! {
 	verify {
 		assert_eq!(
 
-						Pallet::<T>::get_account_currency_restriction_count_delay(
-								sender,
-								CurrencyId::Native,
-						).unwrap(),
-						AllowanceMetadata {
-								allowance_count: 0,
-								current_delay: Some(200u32.into()),
-								once_modifiable_after: None
-						}
+			Pallet::<T>::get_account_currency_restriction_count_delay(
+					sender,
+					CurrencyId::Native,
+			).unwrap(),
+			AllowanceMetadata {
+					allowance_count: 0,
+					current_delay: Some(200u32.into()),
+					once_modifiable_after: None
+			}
 		)
 	}
 
@@ -173,9 +173,9 @@ benchmarks! {
 		assert_eq!(
 
 			Pallet::<T>::get_account_currency_restriction_count_delay(
-					sender,
-					CurrencyId::Native,
-				).unwrap(),
+				sender,
+				CurrencyId::Native,
+			).unwrap(),
 			AllowanceMetadata {
 				allowance_count: 1,
 				current_delay: None,
@@ -191,7 +191,7 @@ benchmarks! {
 		Pallet::<T>::add_allowance_delay(RawOrigin::Signed(sender.clone()).into(), CurrencyId::Native, delay.clone())?;
 		Pallet::<T>::add_transfer_allowance(RawOrigin::Signed(sender.clone()).into(), CurrencyId::Native, receiver.clone().into())?;
 		advance_n_blocks::<T>(1u32.into());
-  }:remove_transfer_allowance(RawOrigin::Signed(sender.clone()), CurrencyId::Native, receiver.clone().into())
+	}:remove_transfer_allowance(RawOrigin::Signed(sender.clone()), CurrencyId::Native, receiver.clone().into())
 	verify {
 		assert_eq!(
 			Pallet::<T>::get_account_currency_transfer_allowance(
@@ -224,16 +224,16 @@ benchmarks! {
 	}
 
 	purge_transfer_allowance_no_remaining_metadata {
-			let (sender, receiver) = set_up_users::<T>();
-			Pallet::<T>::add_transfer_allowance(RawOrigin::Signed(sender.clone()).into(), CurrencyId::Native, receiver.clone().into())?;
-			Pallet::<T>::remove_transfer_allowance(RawOrigin::Signed(sender.clone()).into(), CurrencyId::Native, receiver.clone().into())?;
+		let (sender, receiver) = set_up_users::<T>();
+		Pallet::<T>::add_transfer_allowance(RawOrigin::Signed(sender.clone()).into(), CurrencyId::Native, receiver.clone().into())?;
+		Pallet::<T>::remove_transfer_allowance(RawOrigin::Signed(sender.clone()).into(), CurrencyId::Native, receiver.clone().into())?;
 	}:purge_transfer_allowance(RawOrigin::Signed(sender.clone()), CurrencyId::Native, receiver.clone().into())
 	verify {
 			assert_eq!(
 				Pallet::<T>::get_account_currency_transfer_allowance(
-						(sender.clone(),
-							CurrencyId::Native,
-							Location::from(receiver))
+					(sender.clone(),
+						CurrencyId::Native,
+						Location::from(receiver))
 					),
 			None
 				);
