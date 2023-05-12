ac76fe6133d026b12b4a624e75bbba8be74c65f1
lemunozm <lemunozm@gmail.com>
2023-05-12 09:55:38 +0200

    overdocumented and minor simplification

--- libs/mock-builder/src/storage.rs
+++ libs/mock-builder/src/storage.rs
@@ -1,3 +1,10 @@
+//! This module is in change of storing closures with the type `Fn(I) -> O`
+//! in a static lifetime storage, supporting mixing differents `I` and `O`
+//! types. Because we need to merge different closures with different types in
+//! the same storage, we use an `u128` as closure identification (composed by
+//! the closure function pointer (`u64`) and the pointer to the closure metadata
+//! (`u64`).
+
 use std::{cell::RefCell, collections::HashMap, fmt};
 
 use super::util::TypeSignature;
@@ -6,7 +13,13 @@ use super::util::TypeSignature;
 pub type CallId = u64;
 
 struct CallInfo {
+	/// Closure identification
 	ptr: u128,
+
+	/// Runtime representation of the closure type.
+	/// This field is needed to ensure we are getting the correct closure type,
+	/// since the type at compiler time is lost in the `u128` representation of
+	/// the closure.
 	type_signature: TypeSignature,
 }
 
@@ -17,14 +30,17 @@ thread_local! {
 #[derive(Debug, PartialEq)]
 pub enum Error {
 	CallNotFound,
-	TypeNotMatch(TypeSignature, TypeSignature),
+	TypeNotMatch {
+		expected: TypeSignature,
+		found: TypeSignature,
+	},
 }
 
 impl fmt::Display for Error {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		match self {
 			Error::CallNotFound => write!(f, "Trying to call a function that is not registered"),
-			Error::TypeNotMatch(expected, found) => write!(
+			Error::TypeNotMatch { expected, found } => write!(
 				f,
 				"The function is registered but the type mismatches. Expected {}, found: {}",
 				expected, found
@@ -36,12 +52,23 @@ impl fmt::Display for Error {
 /// Register a call into the call storage.
 /// The registered call can be uniquely identified by the returned `CallId`.
 pub fn register_call<F: Fn(I) -> O + 'static, I, O>(f: F) -> CallId {
+	// We box the closure in order to store it in a fixed place of memory,
+	// and handle it in a more generic way without knowing the specific closure
+	// implementation.
 	let f = Box::new(f) as Box<dyn Fn(I) -> O>;
-	let ptr = Box::into_raw(f);
 
-	// SAFETY: transforming a wide pointer to an u128 is always safe.
+	// We're only interested in the memory address of the closure.
+	// Box is never dropped after this call.
+	let ptr: *const dyn Fn(I) -> O = Box::into_raw(f);
+
 	let call = CallInfo {
+		// We need the transmutation to forget about the type of the closure at compile time,
+		// and then store closures with different types together.
+		// SAFETY: transforming a wide pointer (*const dyn) to an u128 is always safe
+		// because the memory representation is the same.
 		ptr: unsafe { std::mem::transmute(ptr) },
+		// Since we've lost the type representation at compile time, we need to store the type
+		// representation at runtime, in order to recover later the correct closure
 		type_signature: TypeSignature::new::<I, O>(),
 	};
 
@@ -61,27 +88,28 @@ pub fn execute_call<I, O>(call_id: CallId, input: I) -> Result<O, Error> {
 		let registry = &*state.borrow();
 		let call = registry.get(&call_id).ok_or(Error::CallNotFound)?;
 
+		// We need the runtime type check since we lost the type at compile time.
 		if expected_type_signature != call.type_signature {
-			return Err(Error::TypeNotMatch(
-				expected_type_signature,
-				call.type_signature.clone(),
-			));
+			return Err(Error::TypeNotMatch {
+				expected: expected_type_signature,
+				found: call.type_signature.clone(),
+			});
 		}
 
-		// SAFETY: The existence of this boxed closure in consequent calls is ensured
-		// by the forget call below.
-		// The type of the transmuted call is ensured in runtime by the above type
+		// SAFETY:
+		// 1. The existence of this closure ptr in consequent calls is ensured
+		// thanks to Box::into_raw() at register_call(),
+		// which takes the Box ownership without dropping it. So, ptr exists forever.
+		// 2. The type of the transmuted call is ensured in runtime by the above type
 		// signature check.
-		let f = unsafe {
+		// 3. The pointer is correctly aligned because it was allocated by a Box.
+		let f: &dyn Fn(I) -> O = unsafe {
 			#[allow(clippy::useless_transmute)] // Clippy hints something erroneous
-			let ptr: *mut dyn Fn(I) -> O = std::mem::transmute(call.ptr);
-			Box::from_raw(ptr)
+			let ptr: *const dyn Fn(I) -> O = std::mem::transmute(call.ptr);
+			&*ptr
 		};
 
-		let output = f(input);
-
-		std::mem::forget(f);
-		Ok(output)
+		Ok(f(input))
 	})
 }
 
@@ -106,10 +134,10 @@ mod tests {
 
 		assert_eq!(
 			result,
-			Err(Error::TypeNotMatch(
-				TypeSignature::new::<char, usize>(),
-				TypeSignature::new::<u8, usize>()
-			))
+			Err(Error::TypeNotMatch {
+				expected: TypeSignature::new::<char, usize>(),
+				found: TypeSignature::new::<u8, usize>()
+			})
 		);
 	}
 
@@ -121,10 +149,10 @@ mod tests {
 
 		assert_eq!(
 			result,
-			Err(Error::TypeNotMatch(
-				TypeSignature::new::<u8, char>(),
-				TypeSignature::new::<u8, usize>()
-			))
+			Err(Error::TypeNotMatch {
+				expected: TypeSignature::new::<u8, char>(),
+				found: TypeSignature::new::<u8, usize>()
+			})
 		);
 	}
 
