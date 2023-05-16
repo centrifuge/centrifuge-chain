1fe4e58dde1ed0f8b8e7a401a114d51f7d25980e
lemunozm <lemunozm@gmail.com>
2023-05-16 08:21:50 +0200

    add missing license files

--- pallets/loans-ref/src/write_off.rs
+++ pallets/loans-ref/src/write_off.rs
@@ -1,3 +1,16 @@
+// Copyright 2023 Centrifuge Foundation (centrifuge.io).
+// This file is part of Centrifuge chain project.
+
+// Centrifuge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version (see http://www.gnu.org/licenses).
+
+// Centrifuge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
 use cfg_primitives::Moment;
 use cfg_traits::ops::EnsureFixedPointNumber;
 use codec::{Decode, Encode, MaxEncodedLen};
