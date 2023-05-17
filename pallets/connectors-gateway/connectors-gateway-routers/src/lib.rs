f80b70dd8aae79b8b1ac9e8ff56a37ff7ff2f3f1
cdamian <17934949+cdamian@users.noreply.github.com>
2023-05-17 13:00:26 +0200

    connectors-gateway: Rename moonbeam to ethereum_xcm

--- pallets/connectors-gateway/connectors-gateway-routers/src/lib.rs
+++ pallets/connectors-gateway/connectors-gateway-routers/src/lib.rs
@@ -16,9 +16,9 @@ use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::dispatch::DispatchResult;
 use scale_info::TypeInfo;
 
-use crate::moonbeam::EthereumXCMRouter;
+use crate::ethereum_xcm::EthereumXCMRouter;
 
-pub mod moonbeam;
+pub mod ethereum_xcm;
 
 type CurrencyIdOf<T> = <T as pallet_xcm_transactor::Config>::CurrencyId;
 type MessageOf<T> = <T as pallet_connectors_gateway::Config>::Message;
