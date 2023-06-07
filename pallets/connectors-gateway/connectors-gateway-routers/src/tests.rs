cd75b356511ec27bed051bac34a28e420855fbf3
cdamian <17934949+cdamian@users.noreply.github.com>
2023-06-07 15:31:57 +0200

    wip: WIP

--- pallets/connectors-gateway/connectors-gateway-routers/src/tests.rs
+++ pallets/connectors-gateway/connectors-gateway-routers/src/tests.rs
@@ -12,40 +12,65 @@ mod utils {
 
 use utils::*;
 
-mod domain_router {
+mod ethereum_xcm {
 	use cfg_mocks::MessageMock;
 	use cfg_traits::connectors::Router;
+	use frame_support::assert_ok;
+	use pallet_xcm_transactor::RemoteTransactInfoWithMaxWeight;
+	use sp_core::{bounded_vec, H160};
+	use sp_runtime::traits::Convert;
 
 	use super::*;
-	use crate::{
-		axelar_evm::{AxelarEVMRouter, EVMChain, EVMDomain},
-		DomainRouter, FeeValues,
-	};
+	use crate::{ethereum_xcm::EthereumXCMRouter, DomainRouter, XcmDomain};
 
 	#[test]
-	fn axelar_evm_success() {
+	fn success() {
 		new_test_ext().execute_with(|| {
-			let evm_domain = EVMDomain {
-				chain: EVMChain::Ethereum,
-				axelar_contract_address: Default::default(),
-				connectors_contract_address: Default::default(),
-				fee_values: FeeValues {
-					value: Default::default(),
-					gas_limit: 0,
-					max_fee_per_gas: Default::default(),
-					max_priority_fee_per_gas: None,
-				},
+			// Set the correct currency_id and destination to ensure that
+			// `pallet_xcm_transactor::Pallet::transfer_allowed` does not fail.
+
+			let currency_id = CurrencyId::OtherReserve(1);
+			let dest = CurrencyIdToMultiLocation::convert(currency_id.clone()).unwrap();
+
+			let xcm_domain = XcmDomain {
+				location: Box::new(dest.clone().versioned()),
+				ethereum_xcm_transact_call_index: bounded_vec![0],
+				contract_address: H160::from_slice(rand::random::<[u8; 20]>().as_slice()),
+				fee_currency: currency_id,
+				max_gas_limit: 10,
 			};
 
-			let domain_router = DomainRouter::<Runtime>::AxelarEVM(AxelarEVMRouter::<Runtime> {
-				domain: evm_domain,
-				_phantom: Default::default(),
-			});
+			let domain_router =
+				DomainRouter::<Runtime>::EthereumXCM(EthereumXCMRouter::<Runtime> {
+					xcm_domain,
+					_marker: Default::default(),
+				});
+
+			// Required in `pallet_xcm_transactor::Pallet::take_weight_from_transact_info`.
+
+			pallet_xcm_transactor::TransactInfoWithWeightLimit::<Runtime>::insert(
+				dest.clone(),
+				RemoteTransactInfoWithMaxWeight {
+					transact_extra_weight: 1,
+					max_weight: 100_000_000_000,
+					transact_extra_weight_signed: None,
+				},
+			);
+
+			// Required in
+			// `pallet_xcm_transactor::Pallet::take_fee_per_second_from_storage`.
+
+			let fee_per_second = 1u128;
+
+			pallet_xcm_transactor::DestinationAssetFeePerSecond::<Runtime>::insert(
+				dest,
+				fee_per_second,
+			);
 
 			let sender = get_random_test_account_id();
 			let msg = MessageMock::Second;
 
-			domain_router.send(sender, msg).unwrap()
+			assert_ok!(domain_router.send(sender, msg));
 		});
 	}
 }
