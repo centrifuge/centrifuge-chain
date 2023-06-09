ed4841f38d2dbe37719a92095c232c0107b80a37
nuno <hi@nunoalexandre.com>
2023-06-09 13:41:27 +0200

    fmt

--- runtime/altair/src/lib.rs
+++ runtime/altair/src/lib.rs
@@ -59,7 +59,7 @@ use orml_traits::{currency::MutationHooks, parameter_type_with_key};
 use pallet_anchors::AnchorData;
 pub use pallet_balances::Call as BalancesCall;
 use pallet_collective::{EnsureMember, EnsureProportionMoreThan};
-use pallet_ethereum::{Transaction as EthereumTransaction};
+use pallet_ethereum::Transaction as EthereumTransaction;
 use pallet_evm::{Account as EVMAccount, FeeCalculator, Runner};
 use pallet_investments::OrderType;
 use pallet_pool_system::{
@@ -72,10 +72,8 @@ pub use pallet_timestamp::Call as TimestampCall;
 pub use pallet_transaction_payment::{CurrencyAdapter, Multiplier, TargetedFeeAdjustment};
 use pallet_transaction_payment_rpc_runtime_api::{FeeDetails, RuntimeDispatchInfo};
 use polkadot_runtime_common::{prod_or_fast, BlockHashCount, SlowAdjustingFeeUpdate};
+use runtime_common::fees::{DealWithFees, WeightToFee};
 pub use runtime_common::*;
-use runtime_common::{
-	fees::{DealWithFees, WeightToFee},
-};
 use scale_info::TypeInfo;
 use sp_api::impl_runtime_apis;
 use sp_core::{OpaqueMetadata, H160, H256, U256};
@@ -88,9 +86,7 @@ use sp_runtime::{
 		AccountIdConversion, BlakeTwo256, Block as BlockT, ConvertInto, DispatchInfoOf,
 		Dispatchable, PostDispatchInfoOf, UniqueSaturatedInto, Zero,
 	},
-	transaction_validity::{
-		TransactionSource, TransactionValidity, TransactionValidityError,
-	},
+	transaction_validity::{TransactionSource, TransactionValidity, TransactionValidityError},
 	ApplyExtrinsicResult, DispatchError, DispatchResult, FixedI128, Perbill, Permill,
 };
 use sp_std::{marker::PhantomData, prelude::*};
