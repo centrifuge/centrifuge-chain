e680c9649c02c9f9575ff4eb1d5ac057473d5a0d
cdamian <17934949+cdamian@users.noreply.github.com>
2023-06-22 21:13:29 +0200

    runtime: Add pallet-ethereum-transaction

--- runtime/development/src/evm.rs
+++ runtime/development/src/evm.rs
@@ -84,3 +84,5 @@ impl pallet_ethereum::Config for crate::Runtime {
 	type RuntimeEvent = crate::RuntimeEvent;
 	type StateRoot = pallet_ethereum::IntermediateStateRoot<Self>;
 }
+
+impl pallet_ethereum_transaction for crate::Runtime {}
