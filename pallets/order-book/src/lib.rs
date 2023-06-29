106004d33b43537d873f3213289521d25abde7c9
Thea Leake <thea@centrifuge.io>
2023-06-29 10:50:17 +0200

    Init work on new orderbook pallet

---
+++ pallets/order-book/src/lib.rs
@@ -0,0 +1,70 @@
+// Copyright 2023 Centrifuge Foundation (centrifuge.io).
+//
+// This file is part of the Centrifuge chain project.
+// Centrifuge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version (see http://www.gnu.org/licenses).
+// Centrifuge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// This pallet was made using the ZeitGeist Orderbook pallet as a reference;
+// with much of the code being copied or adapted from that pallet.
+// The ZeitGeist Orderbook pallet can be found here: https://github.com/zeitgeistpm/zeitgeist/tree/main/zrml/orderbook-v1
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+//! This module adds an orderbook pallet, allowing oders for currency swaps to
+//! be placed and fulfilled for currencies in an asset registry.
+
+#[cfg(test)]
+pub(crate) mod mock;
+pub use pallet::*;
+
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	use super::*;
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	/// The current storage version.
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	#[pallet::storage_version(STORAGE_VERSION)]
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	pub type DepositBalanceOf<T> = <<T as Config>::ReserveCurrency as Currency<
+		<T as frame_system::Config>::AccountId,
+	>>::Balance;
+
+	type ReserveCurrency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
+	/// Id type of Currency Exchanges will take plae for
+	type CurrencyId: AssetId
+		+ Parameter
+		+ Debug
+		+ Default
+		+ Member
+		+ Copy
+		+ MaybeSerializeDeserialize
+		+ Ord
+		+ TypeInfo
+		+ MaxEncodedLen;
+
+	type TradeableShares: MultiReservableCurrency<
+		Self::AccountId,
+		Balance = DepositBalanceOf<Self>,
+		CurrencyId = CurrencyId,
+	>;
+}
---
+++ pallets/order-book/src/mock.rs
@@ -0,0 +1,138 @@
+// Copyright 2023 Centrifuge Foundation (centrifuge.io).
+//
+// This file is part of the Centrifuge chain project.
+// Centrifuge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version (see http://www.gnu.org/licenses).
+// Centrifuge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+use core::default;
+
+use cfg_types::tokens::CustomMetadata;
+use codec::{Decode, Encode, MaxEncodedLen};
+use frame_support::{
+	parameter_types,
+	traits::{ConstU32, ConstU64},
+	Deserialize, Serialize,
+};
+use scale_info::TypeInfo;
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
+
+use crate as order_book;
+
+pub(crate) const ORDER_PLACER_0: u64 = 0x1;
+
+type Balance = u64;
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
+pub type MockAccountId = u64;
+
+frame_support::construct_runtime!(
+	  pub enum Runtime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	  {
+			Balances: pallet_balances,
+			System: frame_system,
+		  OrderBook: order_book,
+	  }
+);
+
+parameter_types! {
+	  pub const BlockHashCount: u64 = 250;
+	  pub const SS58Prefix: u8 = 42;
+}
+
+impl frame_system::Config for Runtime {
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type AccountId = MockAccountId;
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockHashCount = BlockHashCount;
+	type BlockLength = ();
+	type BlockNumber = u64;
+	type BlockWeights = ();
+	type DbWeight = ();
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type Header = Header;
+	type Index = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+	type OnKilledAccount = ();
+	type OnNewAccount = ();
+	type OnSetCode = ();
+	type PalletInfo = PalletInfo;
+	type RuntimeCall = RuntimeCall;
+	type RuntimeEvent = RuntimeEvent;
+	type RuntimeOrigin = RuntimeOrigin;
+	type SS58Prefix = SS58Prefix;
+	type SystemWeightInfo = ();
+	type Version = ();
+}
+
+#[derive(
+	Clone,
+	Copy,
+	Debug,
+	Default,
+	PartialOrd,
+	Ord,
+	Encode,
+	Decode,
+	Eq,
+	PartialEq,
+	MaxEncodedLen,
+	TypeInfo,
+	Deserialize,
+	Serialize,
+)]
+pub enum CurrencyId {
+	#[default]
+	A,
+	B,
+	C,
+	D,
+}
+
+impl pallet_balances::Config for Runtime {
+	type AccountStore = System;
+	type Balance = u64;
+	type DustRemoval = ();
+	type ExistentialDeposit = ConstU64<1>;
+	type MaxLocks = ();
+	type MaxReserves = ConstU32<50>;
+	type ReserveIdentifier = [u8; 8];
+	type RuntimeEvent = RuntimeEvent;
+	type WeightInfo = ();
+}
+
+cfg_test_utils::mocks::orml_asset_registry::impl_mock_registry! {
+		RegistryMock,
+		CurrencyId,
+		Balance,
+		CustomMetadata
+}
+
+impl order_book::Config for Runtime {
+	// type AssetRegistry = RegistryMock;
+	// type Balance = Balance;
+	// type CurrencyId = CurrencyId;
+	// type RuntimeEvent = RuntimeEvent;
+}
+
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	sp_io::TestExternalities::new(
+		frame_system::GenesisConfig::default()
+			.build_storage::<Runtime>()
+			.unwrap(),
+	)
+}
