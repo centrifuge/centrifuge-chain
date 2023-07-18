90f6de605aef90c29558131c93bac8bd0b21bd95
Thea Leake <thea@centrifuge.io>
2023-07-18 13:40:49 +0200

    Add remaining storage items to existing tests

--- pallets/order-book/src/tests.rs
+++ pallets/order-book/src/tests.rs
@@ -28,6 +28,24 @@ fn create_order_v1_works() {
 				min_fullfillment_amount: 100,
 				max_sell_amount: 1000
 			})
+		);
+		assert_eq!(
+			UserOrders::<Runtime>::get(ACCOUNT_0, order_id),
+			Ok(Order {
+				order_id: order_id,
+				placing_account: ACCOUNT_0,
+				asset_in_id: CurrencyId::A,
+				asset_out_id: CurrencyId::B,
+				buy_amount: 100,
+				initial_buy_amount: 100,
+				price: 10,
+				min_fullfillment_amount: 100,
+				max_sell_amount: 1000
+			})
+		);
+		assert_eq!(
+			AssetPairOrders::<Runtime>::get(CurrencyId::A, CurrencyId::B),
+			vec![order_id,]
 		)
 	})
 }
@@ -47,6 +65,20 @@ fn user_cancel_order_works() {
 			RuntimeOrigin::signed(ACCOUNT_0),
 			order_id
 		));
+		assert_err!(
+			Orders::<Runtime>::get(order_id),
+			Error::<Runtime>::OrderNotFound
+		);
+
+		assert_err!(
+			UserOrders::<Runtime>::get(ACCOUNT_0, order_id),
+			Error::<Runtime>::OrderNotFound
+		);
+
+		assert_eq!(
+			AssetPairOrders::<Runtime>::get(CurrencyId::A, CurrencyId::B),
+			vec![]
+		)
 	})
 }
 
@@ -76,6 +108,26 @@ fn place_order_works() {
 				min_fullfillment_amount: 100,
 				max_sell_amount: 1000
 			})
+		);
+
+		assert_eq!(
+			UserOrders::<Runtime>::get(ACCOUNT_0, order_id),
+			Ok(Order {
+				order_id: order_id,
+				placing_account: ACCOUNT_0,
+				asset_in_id: CurrencyId::A,
+				asset_out_id: CurrencyId::B,
+				buy_amount: 100,
+				initial_buy_amount: 100,
+				price: 10,
+				min_fullfillment_amount: 100,
+				max_sell_amount: 1000
+			})
+		);
+
+		assert_eq!(
+			AssetPairOrders::<Runtime>::get(CurrencyId::A, CurrencyId::B),
+			vec![order_id,]
 		)
 	})
 }
