2e89a070228fd5b233155b0d5ee2886ba9889ec3
nuno <hi@nunoalexandre.com>
2023-08-03 10:50:51 +0200

    clippy

--- runtime/altair/src/migrations.rs
+++ runtime/altair/src/migrations.rs
@@ -11,10 +11,10 @@
 // GNU General Public License for more details.
 use cfg_primitives::Balance;
 use cfg_types::tokens::CurrencyId;
-use codec::{Decode, Encode};
 #[cfg(feature = "try-runtime")]
 use frame_support::ensure;
 use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
+#[cfg(feature = "try-runtime")]
 use sp_std::vec::Vec;
 
 use crate::Runtime;
