d2473879625bff41d83fb26ed18b7385336b7d1e
Thea Leake <thea@centrifuge.io>
2023-08-07 18:11:04 +0200

    Switch var result handling from if/let to ?

--- runtime/development/src/lib.rs
+++ runtime/development/src/lib.rs
@@ -2339,20 +2339,16 @@ impl_runtime_apis! {
 
 	impl runtime_common::apis::InvestmentsApi<Block, AccountId, TrancheCurrency, CurrencyId, PoolId, Balance> for Runtime {
 		fn investment_portfolio(account_id: AccountId) -> Option<Vec<(PoolId, CurrencyId, TrancheCurrency, Balance)>> {
-			if let Ok(account_investments) = Investments::get_account_investments_currency(&account_id) {
-
-				// Pool getting defined in runtime
-				// as opposed to pallet helper method
-				// as getting pool id in investments pallet
-				// would force tighter coupling of investments
-				// and pool pallets.
-				let portfolio: Vec<(PoolId, CurrencyId, TrancheCurrency, Balance)> = account_investments.into_iter().map(|a_i| {
-						(a_i.0.of_pool(), a_i.1, a_i.0, a_i.2)
-				}).collect();
-				Some(portfolio)
-			} else {
-				None
-			}
+			let account_investments = Investments::get_account_investments_currency(&account_id).ok()?;
+			// Pool getting defined in runtime
+			// as opposed to pallet helper method
+			// as getting pool id in investments pallet
+			// would force tighter coupling of investments
+			// and pool pallets.
+			let portfolio: Vec<(PoolId, CurrencyId, TrancheCurrency, Balance)> = account_investments.into_iter().map(|a_i| {
+					(a_i.0.of_pool(), a_i.1, a_i.0, a_i.2)
+			}).collect();
+			Some(portfolio)
 		}
 	}
 
