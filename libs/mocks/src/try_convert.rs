0a6fbc9a01449f342df6eaaf93095659d9463693
Frederik Gartenmeister <mustermeiszer@posteo.de>
2023-09-01 13:32:25 +0200

    feat: tests for LpRelayerInstance converter

--- libs/mocks/src/try_convert.rs
+++ libs/mocks/src/try_convert.rs
@@ -2,10 +2,10 @@
 pub mod pallet {
 	use cfg_traits::TryConvert;
 	use frame_support::pallet_prelude::*;
-	use mock_builder::{execute_call, register_call};
+	use mock_builder::{execute_call_instance, register_call_instance};
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
+	pub trait Config<I: 'static = ()>: frame_system::Config {
 		type From;
 
 		type To;
@@ -15,27 +15,27 @@ pub mod pallet {
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
+	pub struct Pallet<T, I = ()>(_);
 
 	#[pallet::storage]
-	pub(super) type CallIds<T: Config> = StorageMap<
+	pub(super) type CallIds<T: Config<I>, I: 'static = ()> = StorageMap<
 		_,
 		Blake2_128Concat,
 		<Blake2_128 as frame_support::StorageHasher>::Output,
 		mock_builder::CallId,
 	>;
 
-	impl<T: Config> Pallet<T> {
+	impl<T: Config<I>, I: 'static> Pallet<T, I> {
 		pub fn mock_try_convert(f: impl Fn(T::From) -> Result<T::To, T::Error> + 'static) {
-			register_call!(move |from| f(from));
+			register_call_instance!(move |from| f(from));
 		}
 	}
 
-	impl<T: Config> TryConvert<T::From, T::To> for Pallet<T> {
+	impl<T: Config<I>, I: 'static> TryConvert<T::From, T::To> for Pallet<T, I> {
 		type Error = T::Error;
 
 		fn try_convert(from: T::From) -> Result<T::To, Self::Error> {
-			execute_call!(from)
+			execute_call_instance!(from)
 		}
 	}
 }
--- runtime/common/src/xcm.rs
+++ runtime/common/src/xcm.rs
@@ -123,6 +123,317 @@ where
 
 #[cfg(test)]
 mod test {
+	use cfg_mocks::{
+		pallet_mock_liquidity_pools, pallet_mock_routers, pallet_mock_try_convert, MessageMock,
+		RouterMock,
+	};
+	use frame_support::{assert_ok, traits::EnsureOrigin};
+	use frame_system::EnsureRoot;
+	use pallet_liquidity_pools_gateway::{EnsureLocal, GatewayOrigin};
+	use sp_core::{ConstU16, ConstU32, ConstU64, H256};
+	use sp_runtime::{
+		testing::Header,
+		traits::{BlakeTwo256, IdentityLookup},
+		DispatchError,
+	};
+	use xcm_executor::traits::ConvertOrigin;
+
+	use super::*;
+
+	type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+	type Block = frame_system::mocking::MockBlock<Runtime>;
+	type AccountId = u64;
+
+	pub fn new_test_ext() -> sp_io::TestExternalities {
+		frame_system::GenesisConfig::default()
+			.build_storage::<Runtime>()
+			.unwrap()
+			.into()
+	}
+
+	// For testing the pallet, we construct a mock runtime.
+	frame_support::construct_runtime!(
+		pub enum Runtime where
+			Block = Block,
+			NodeBlock = Block,
+			UncheckedExtrinsic = UncheckedExtrinsic,
+		{
+			System: frame_system,
+			Gateway: pallet_liquidity_pools_gateway,
+			MockLP: pallet_mock_liquidity_pools,
+			MockParaAsEvmChain: pallet_mock_try_convert::<Instance1>,
+			MockOriginRecovery: pallet_mock_try_convert::<Instance2>,
+		}
+	);
+
+	impl frame_system::Config for Runtime {
+		type AccountData = ();
+		type AccountId = AccountId;
+		type BaseCallFilter = frame_support::traits::Everything;
+		type BlockHashCount = ConstU64<250>;
+		type BlockLength = ();
+		type BlockNumber = u64;
+		type BlockWeights = ();
+		type DbWeight = ();
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type Header = Header;
+		type Index = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type MaxConsumers = ConstU32<16>;
+		type OnKilledAccount = ();
+		type OnNewAccount = ();
+		type OnSetCode = ();
+		type PalletInfo = PalletInfo;
+		type RuntimeCall = RuntimeCall;
+		type RuntimeEvent = RuntimeEvent;
+		type RuntimeOrigin = RuntimeOrigin;
+		type SS58Prefix = ConstU16<42>;
+		type SystemWeightInfo = ();
+		type Version = ();
+	}
+
+	impl pallet_mock_try_convert::Config<pallet_mock_try_convert::Instance1> for Runtime {
+		type Error = ();
+		type From = ParaId;
+		type To = EVMChainId;
+	}
+
+	impl pallet_mock_try_convert::Config<pallet_mock_try_convert::Instance2> for Runtime {
+		type Error = DispatchError;
+		type From = (Vec<u8>, Vec<u8>);
+		type To = DomainAddress;
+	}
+
+	impl pallet_mock_liquidity_pools::Config for Runtime {
+		type DomainAddress = DomainAddress;
+		type Message = MessageMock;
+	}
+
+	impl pallet_mock_routers::Config for Runtime {}
+
+	impl pallet_liquidity_pools_gateway::Config for Runtime {
+		type AdminOrigin = EnsureRoot<AccountId>;
+		type InboundQueue = MockLP;
+		type LocalEVMOrigin = pallet_liquidity_pools_gateway::EnsureLocal;
+		type MaxIncomingMessageSize = ConstU32<1024>;
+		type Message = MessageMock;
+		type OriginRecovery = MockOriginRecovery;
+		type Router = RouterMock<Runtime>;
+		type RuntimeEvent = RuntimeEvent;
+		type RuntimeOrigin = RuntimeOrigin;
+		type WeightInfo = ();
+	}
+
+	const RELAYER_PARA_ID: u32 = 1000;
+	const RELAYER_EVM_ID: u64 = 1001;
+	const RELAYER_ADDRESS: [u8; 20] = [1u8; 20];
+
 	#[test]
-	fn lp_gatway_converts_correctly() {}
+	fn lp_instance_relayer_converts_correctly() {
+		new_test_ext().execute_with(|| {
+			let expected_address = DomainAddress::EVM(RELAYER_EVM_ID, RELAYER_ADDRESS);
+
+			assert_ok!(Gateway::add_relayer(
+				RuntimeOrigin::root(),
+				expected_address.clone(),
+			));
+
+			MockParaAsEvmChain::mock_try_convert(|from| {
+				assert_eq!(from, RELAYER_PARA_ID);
+				Ok(RELAYER_EVM_ID)
+			});
+
+			let location: MultiLocation = MultiLocation::new(
+				1,
+				X2(
+					Parachain(RELAYER_PARA_ID),
+					AccountKey20 {
+						network: None,
+						key: RELAYER_ADDRESS,
+					},
+				),
+			);
+
+			let origin = LpInstanceRelayer::<MockParaAsEvmChain, Runtime>::convert_origin(
+				location,
+				OriginKind::SovereignAccount,
+			)
+			.expect("Origin conversion failed unexpectedly.");
+
+			assert_eq!(
+				EnsureLocal::ensure_origin(origin).expect("Generate origin must be GatewayOrigin"),
+				GatewayOrigin::AxelarRelay(expected_address)
+			)
+		})
+	}
+
+	#[test]
+	fn lp_instance_relayer_fails_with_wrong_location() {
+		new_test_ext().execute_with(|| {
+			let expected_address = DomainAddress::EVM(RELAYER_EVM_ID, RELAYER_ADDRESS);
+
+			assert_ok!(Gateway::add_relayer(
+				RuntimeOrigin::root(),
+				expected_address.clone(),
+			));
+
+			MockParaAsEvmChain::mock_try_convert(|from| {
+				assert_eq!(from, RELAYER_PARA_ID);
+				Ok(RELAYER_EVM_ID)
+			});
+
+			let location: MultiLocation = MultiLocation::new(1, X1(Parachain(RELAYER_PARA_ID)));
+
+			assert_eq!(
+				LpInstanceRelayer::<MockParaAsEvmChain, Runtime>::convert_origin(
+					location,
+					OriginKind::SovereignAccount,
+				)
+				.unwrap_err(),
+				location
+			);
+		})
+	}
+
+	#[test]
+	fn lp_instance_relayer_fails_if_relayer_not_set() {
+		new_test_ext().execute_with(|| {
+			MockParaAsEvmChain::mock_try_convert(|from| {
+				assert_eq!(from, RELAYER_PARA_ID);
+				Ok(RELAYER_EVM_ID)
+			});
+
+			let location: MultiLocation = MultiLocation::new(
+				1,
+				X2(
+					Parachain(RELAYER_PARA_ID),
+					AccountKey20 {
+						network: None,
+						key: RELAYER_ADDRESS,
+					},
+				),
+			);
+
+			assert_eq!(
+				LpInstanceRelayer::<MockParaAsEvmChain, Runtime>::convert_origin(
+					location,
+					OriginKind::SovereignAccount,
+				)
+				.unwrap_err(),
+				location
+			);
+		})
+	}
+
+	#[test]
+	fn lp_instance_relayer_fails_if_para_to_evm_fails() {
+		new_test_ext().execute_with(|| {
+			let expected_address = DomainAddress::EVM(RELAYER_EVM_ID, RELAYER_ADDRESS);
+
+			assert_ok!(Gateway::add_relayer(
+				RuntimeOrigin::root(),
+				expected_address.clone(),
+			));
+
+			MockParaAsEvmChain::mock_try_convert(|from| {
+				assert_eq!(from, RELAYER_PARA_ID);
+				Err(())
+			});
+
+			let location: MultiLocation = MultiLocation::new(
+				1,
+				X2(
+					Parachain(RELAYER_PARA_ID),
+					AccountKey20 {
+						network: None,
+						key: RELAYER_ADDRESS,
+					},
+				),
+			);
+
+			assert_eq!(
+				LpInstanceRelayer::<MockParaAsEvmChain, Runtime>::convert_origin(
+					location,
+					OriginKind::SovereignAccount,
+				)
+				.unwrap_err(),
+				location
+			);
+		})
+	}
+
+	#[test]
+	fn lp_instance_relayer_fails_if_wrong_para() {
+		new_test_ext().execute_with(|| {
+			let expected_address = DomainAddress::EVM(RELAYER_EVM_ID, RELAYER_ADDRESS);
+
+			assert_ok!(Gateway::add_relayer(
+				RuntimeOrigin::root(),
+				expected_address.clone(),
+			));
+
+			MockParaAsEvmChain::mock_try_convert(|from| {
+				assert_eq!(from, 1);
+				Err(())
+			});
+
+			let location: MultiLocation = MultiLocation::new(
+				1,
+				X2(
+					Parachain(1),
+					AccountKey20 {
+						network: None,
+						key: RELAYER_ADDRESS,
+					},
+				),
+			);
+
+			assert_eq!(
+				LpInstanceRelayer::<MockParaAsEvmChain, Runtime>::convert_origin(
+					location,
+					OriginKind::SovereignAccount,
+				)
+				.unwrap_err(),
+				location
+			);
+		})
+	}
+
+	#[test]
+	fn lp_instance_relayer_fails_if_wrong_address() {
+		new_test_ext().execute_with(|| {
+			let expected_address = DomainAddress::EVM(RELAYER_EVM_ID, RELAYER_ADDRESS);
+
+			assert_ok!(Gateway::add_relayer(
+				RuntimeOrigin::root(),
+				expected_address.clone(),
+			));
+
+			MockParaAsEvmChain::mock_try_convert(|from| {
+				assert_eq!(from, RELAYER_PARA_ID);
+				Ok(RELAYER_EVM_ID)
+			});
+
+			let location: MultiLocation = MultiLocation::new(
+				1,
+				X2(
+					Parachain(RELAYER_PARA_ID),
+					AccountKey20 {
+						network: None,
+						key: [0u8; 20],
+					},
+				),
+			);
+
+			assert_eq!(
+				LpInstanceRelayer::<MockParaAsEvmChain, Runtime>::convert_origin(
+					location,
+					OriginKind::SovereignAccount,
+				)
+				.unwrap_err(),
+				location
+			);
+		})
+	}
 }
