40f4cfa26d4ae47211477699f2101e88b24fd42f
Frederik Gartenmeister <mustermeiszer@posteo.de>
2023-09-08 14:45:42 +0200

    feat: bumb version, fix taplo

--- runtime/altair/src/lib.rs
+++ runtime/altair/src/lib.rs
@@ -127,7 +127,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("altair"),
 	impl_name: create_runtime_str!("altair"),
 	authoring_version: 1,
-	spec_version: 1031,
+	spec_version: 1032,
 	impl_version: 1,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
