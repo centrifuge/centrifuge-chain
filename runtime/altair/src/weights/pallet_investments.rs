e72dfe0ce715df4ce62668e9a70c992dff1484ea
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2023-09-25 15:44:05 +0200

    Investments: benchmarks (#1563)
    * basic benchmark working
    
    * migrated to benchmarks::v2
    
    * updating invest/redeem orders working
    
    * add collected benchmarks
    
    * add benchmarks to runtimes
    
    * use correct new weights
    
    * minor fix
    
    * initializing pool
    
    * bypass account permission for benchmarking
    
    * minor fix
    
    * remove default requirement for investment_id

---
+++ runtime/altair/src/weights/pallet_investments.rs
@@ -0,0 +1,23 @@
+//! TEMP: This file will be regenerated!
+
+use frame_support::weights::Weight;
+use sp_std::marker::PhantomData;
+
+pub struct WeightInfo<T>(PhantomData<T>);
+impl<T: frame_system::Config> pallet_investments::WeightInfo for WeightInfo<T> {
+	fn update_invest_order() -> Weight {
+		Weight::zero()
+	}
+
+	fn update_redeem_order() -> Weight {
+		Weight::zero()
+	}
+
+	fn collect_investments(_: u32) -> Weight {
+		Weight::zero()
+	}
+
+	fn collect_redemptions(_: u32) -> Weight {
+		Weight::zero()
+	}
+}
