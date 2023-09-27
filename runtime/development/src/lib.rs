477b3429db1da07c3571f498ffb01455a3c337be
William Freudenberger <w.freude@icloud.com>
2023-09-27 16:11:49 +0200

    chore: bump dev spec_version

--- runtime/development/src/lib.rs
+++ runtime/development/src/lib.rs
@@ -138,7 +138,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("centrifuge-devel"),
 	impl_name: create_runtime_str!("centrifuge-devel"),
 	authoring_version: 1,
-	spec_version: 1030,
+	spec_version: 1031,
 	impl_version: 1,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
