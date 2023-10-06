160318c7189f6e35814f72768c62de6c866d3476
William Freudenberger <w.freude@icloud.com>
2023-10-06 18:40:17 +0200

    fix: remove spec_version check for Centrifuge 1023 migrations (#1584)

--- runtime/development/src/lib.rs
+++ runtime/development/src/lib.rs
@@ -139,7 +139,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("centrifuge-devel"),
 	impl_name: create_runtime_str!("centrifuge-devel"),
 	authoring_version: 1,
-	spec_version: 1031,
+	spec_version: 1032,
 	impl_version: 1,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
