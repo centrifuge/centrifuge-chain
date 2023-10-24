2c892887cce3308265279490e60fec62037e900c
nuno <hi@nunoalexandre.com>
2023-10-24 09:58:48 +0200

    fmt

--- pallets/liquidity-pools/src/lib.rs
+++ pallets/liquidity-pools/src/lib.rs
@@ -453,7 +453,7 @@ pub mod pallet {
 			// TODO(future): Once we diverge from 1-to-1 conversions for foreign and pool
 			// currencies, this price must be first converted into the currency_id and then
 			// re-denominated to 18 decimals (i.e. `Ratio` precision)
-			let price_value  = T::TrancheTokenPrice::get(pool_id, tranche_id)
+			let price_value = T::TrancheTokenPrice::get(pool_id, tranche_id)
 				.ok_or(Error::<T>::MissingTranchePrice)?;
 
 			// Check that the registered asset location matches the destination
