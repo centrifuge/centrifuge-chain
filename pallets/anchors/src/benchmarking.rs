5ee2a481da389529d367805ccdb6d3569fc9a2bd
William Freudenberger <w.freude@icloud.com>
2023-10-31 08:10:54 +0100

    bench: fix anchors

--- pallets/anchors/src/benchmarking.rs
+++ pallets/anchors/src/benchmarking.rs
@@ -10,7 +10,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
-use frame_support::traits::Get;
+use frame_support::traits::{tokens::Balance, Get};
 use frame_system::RawOrigin;
 use sp_runtime::traits::One;
 
@@ -30,6 +30,7 @@ const PROOF: [u8; 32] = [
 	192, 195, 141, 209, 99, 91, 39, 154, 243, 6, 188, 4, 144, 5, 89, 252, 52, 105, 112, 173, 143,
 	101, 65, 6, 191, 206, 210, 2, 176, 103, 161, 14,
 ];
+const MICRO_CFG: u64 = 1_000_000_000_000;
 
 fn day<T: From<u64>>(n: u64) -> T {
 	T::from(common::MILLISECS_PER_DAY * n + 1)
@@ -49,6 +50,7 @@ benchmarks! {
 		T: Config + pallet_aura::Config,
 		T::Hash: From<[u8; 32]>,
 		T::Moment: From<u64>,
+		BalanceOf<T>: From<u64>,
 	}
 
 	pre_commit {
@@ -58,7 +60,7 @@ benchmarks! {
 		let caller = whitelisted_caller();
 
 		let required_deposit = T::Fees::fee_value(T::PreCommitDepositFeeKey::get());
-		T::Currency::make_free_balance_be(&caller, required_deposit);
+		T::Currency::make_free_balance_be(&caller, required_deposit + MICRO_CFG.into() );
 
 		let anchor_id = T::Hashing::hash_of(&0);
 
@@ -73,7 +75,7 @@ benchmarks! {
 
 		let caller = whitelisted_caller();
 		let required_deposit = T::Fees::fee_value(T::PreCommitDepositFeeKey::get());
-		T::Currency::make_free_balance_be(&caller, required_deposit);
+		T::Currency::make_free_balance_be(&caller, required_deposit + MICRO_CFG.into() );
 
 		let pre_image = T::Hashing::hash_of(&0);
 		let anchor_id = pre_image.using_encoded(T::Hashing::hash);
@@ -90,7 +92,7 @@ benchmarks! {
 		config_mocks();
 
 		let required_deposit = T::Fees::fee_value(T::PreCommitDepositFeeKey::get());
-		T::Currency::make_free_balance_be(&caller, required_deposit);
+		T::Currency::make_free_balance_be(&caller, required_deposit + MICRO_CFG.into() );
 
 		assert!(<PreCommits<T>>::get(anchor_id).is_none());
 		assert!(<AnchorEvictDates<T>>::get(anchor_id).is_some());
@@ -105,7 +107,7 @@ benchmarks! {
 
 		let anchor_ids = (0..EVICT_PRE_COMMIT_LIST_SIZE)
 			.map(|i| {
-				T::Currency::make_free_balance_be(&caller, required_deposit);
+				T::Currency::make_free_balance_be(&caller, required_deposit + MICRO_CFG.into() );
 
 				let anchor_id = T::Hashing::hash_of(&i);
 
