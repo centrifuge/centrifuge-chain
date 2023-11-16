914bc93fdd114efa258ccda1720d90a1f4895c3a
cdamian <17934949+cdamian@users.noreply.github.com>
2023-11-16 12:01:31 +0100

    integration-tests: Change default log level to ERROR

--- runtime/integration-tests/src/utils/logs.rs
+++ runtime/integration-tests/src/utils/logs.rs
@@ -21,7 +21,7 @@ const UNINITIALIZED: usize = 0;
 const INITIALIZING: usize = 1;
 const INITIALIZED: usize = 2;
 
-const LOG_LEVEL: LevelFilter = LevelFilter::INFO;
+const LOG_LEVEL: LevelFilter = LevelFilter::ERROR;
 
 pub fn init_logs() {
 	if GLOBAL_INIT
