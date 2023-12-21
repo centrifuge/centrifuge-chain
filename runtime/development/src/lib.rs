5fa9a847416c13e8ca8d808e29db008baebebd4f
lemunozm <lemunozm@gmail.com>
2023-12-21 14:44:07 +0100

    loans using new oracle v2 system in runtimes

--- runtime/development/src/lib.rs
+++ runtime/development/src/lib.rs
@@ -32,7 +32,6 @@ use cfg_types::{
 	consts::pools::*,
 	fee_keys::{Fee, FeeKey},
 	fixed_point::{Quantity, Rate, Ratio},
-	ids::PRICE_ORACLE_PALLET_ID,
 	investments::InvestmentPortfolio,
 	locations::Location,
 	oracles::OracleKey,
@@ -93,7 +92,6 @@ use runtime_common::{
 	fees::{DealWithFees, FeeToTreasury, WeightToFee},
 	oracle::{Feeder, OracleConverterBridge},
 	permissions::PoolAdminCheck,
-	production_or_benchmark,
 	xcm::AccountIdToMultiLocation,
 	xcm_transactor, AllowanceDeposit, CurrencyED, HoldId, NativeCurrency,
 };
@@ -1307,46 +1305,21 @@ impl pallet_xcm_transactor::Config for Runtime {
 
 parameter_types! {
 	pub const MaxActiveLoansPerPool: u32 = 1000;
-	pub const MaxRateCount: u32 = MaxActiveLoansPerPool::get();
-	pub const MaxCollectionSize: u32 = MaxActiveLoansPerPool::get();
+	pub const MaxRateCount: u32 = 1000; // See #1024
+	pub const FirstValueFee: Fee = Fee::Balance(deposit(1, pallet_oracle_feed::util::size_of_feed::<Runtime>()));
+										//
 	#[derive(Clone, PartialEq, Eq, Debug, TypeInfo, Encode, Decode, MaxEncodedLen)]
 	pub const MaxWriteOffPolicySize: u32 = 10;
-	pub const MaxPriceOracleMembers: u32 = 5;
-	pub const MaxHasDispatchedSize: u32 = production_or_benchmark!(
-		MaxPriceOracleMembers::get(),
-		// For benchmarking we need a number of members equal to the active loans.
-		// The benchmark distinction can be removed once
-		// <https://github.com/open-web3-stack/open-runtime-module-library/issues/920> is merged.
-		MaxActiveLoansPerPool::get()
-	);
-	pub const MaxPoolsWithExternalPrices: u32 = 50;
-	pub RootOperatorOraclePrice: AccountId = PRICE_ORACLE_PALLET_ID.into_account_truncating();
-}
 
-impl pallet_membership::Config for Runtime {
-	type AddOrigin = EnsureRootOr<HalfOfCouncil>;
-	type MaxMembers = MaxPriceOracleMembers;
-	type MembershipChanged = PriceOracle;
-	type MembershipInitialized = ();
-	type PrimeOrigin = EnsureRootOr<HalfOfCouncil>;
-	type RemoveOrigin = EnsureRootOr<HalfOfCouncil>;
-	type ResetOrigin = EnsureRootOr<HalfOfCouncil>;
-	type RuntimeEvent = RuntimeEvent;
-	type SwapOrigin = EnsureRootOr<HalfOfCouncil>;
-	type WeightInfo = pallet_membership::weights::SubstrateWeight<Self>;
-}
-
-parameter_types! {
 	#[derive(Clone, PartialEq, Eq, Debug, TypeInfo, Encode, Decode, MaxEncodedLen)]
 	pub const MaxFeedersPerKey: u32 = 10;
-	pub const FirstValueFee: Fee = Fee::Balance(deposit(1, pallet_oracle_feed::util::size_of_feed::<Runtime>()));
 }
 
 impl pallet_oracle_feed::Config for Runtime {
 	type FeederOrigin = EitherOfDiverse<EnsureRoot<AccountId>, EnsureSigned<AccountId>>;
 	type FirstValuePayFee = FeeToTreasury<Fees, FirstValueFee>;
 	type OracleKey = OracleKey;
-	type OracleValue = Quantity;
+	type OracleValue = Ratio;
 	type RuntimeEvent = RuntimeEvent;
 	type Time = Timestamp;
 	type WeightInfo = weights::pallet_oracle_feed::WeightInfo<Self>;
@@ -1370,44 +1343,8 @@ impl pallet_oracle_data_collection::Config for Runtime {
 	type WeightInfo = weights::pallet_oracle_data_collection::WeightInfo<Self>;
 }
 
-parameter_types! {
-	pub const MaxFeedValues: u32 = 500;
-}
-
-impl orml_oracle::Config for Runtime {
-	type CombineData = runtime_common::oracle::LastOracleValue;
-	type MaxFeedValues = MaxFeedValues;
-	type MaxHasDispatchedSize = MaxHasDispatchedSize;
-	#[cfg(not(feature = "runtime-benchmarks"))]
-	type Members = PriceOracleMembership;
-	// This can be removed once
-	// <https://github.com/open-web3-stack/open-runtime-module-library/issues/920> is merged.
-	#[cfg(feature = "runtime-benchmarks")]
-	type Members = runtime_common::oracle::benchmarks_util::Members;
-	type OnNewData = runtime_common::oracle::OnNewPrice<PriceCollector>;
-	type OracleKey = OracleKey;
-	type OracleValue = Quantity;
-	type RootOperatorAccountId = RootOperatorOraclePrice;
-	type RuntimeEvent = RuntimeEvent;
-	type Time = Timestamp;
-	type WeightInfo = ();
-}
-
-impl pallet_data_collector::Config for Runtime {
-	type CollectionId = PoolId;
-	type Data = Balance;
-	type DataId = OracleKey;
-	type DataProvider =
-		runtime_common::oracle::DataProviderBridge<PriceOracle, OrmlAssetRegistry, PoolSystem>;
-	type MaxCollectionSize = MaxCollectionSize;
-	type MaxCollections = MaxPoolsWithExternalPrices;
-	type Moment = Millis;
-}
-
 impl pallet_interest_accrual::Config for Runtime {
 	type Balance = Balance;
-	// TODO: This is a stopgap value until we can calculate it correctly with
-	// updated benchmarks. See #1024
 	type MaxRateCount = MaxRateCount;
 	type Rate = Rate;
 	type RuntimeEvent = RuntimeEvent;
@@ -1432,7 +1369,7 @@ impl pallet_loans::Config for Runtime {
 	type Pool = PoolSystem;
 	type PoolId = PoolId;
 	type PriceId = OracleKey;
-	type PriceRegistry = PriceCollector;
+	type PriceRegistry = OraclePriceCollection;
 	type Quantity = Quantity;
 	type Rate = Rate;
 	type RuntimeChange = runtime_common::changes::RuntimeChange<Runtime, FastDelay>;
@@ -1946,7 +1883,6 @@ construct_runtime!(
 		BlockRewardsBase: pallet_rewards::<Instance2>::{Pallet, Storage, Event<T>, Config<T>} = 110,
 		BlockRewards: pallet_block_rewards::{Pallet, Call, Storage, Event<T>, Config<T>} = 111,
 		TransferAllowList: pallet_transfer_allowlist::{Pallet, Call, Storage, Event<T>} = 112,
-		PriceCollector: pallet_data_collector::{Pallet, Storage} = 113,
 		GapRewardMechanism: pallet_rewards::mechanism::gap = 114,
 		LiquidityPoolsGateway: pallet_liquidity_pools_gateway::{Pallet, Call, Storage, Event<T>, Origin } = 115,
 		OrderBook: pallet_order_book::{Pallet, Call, Storage, Event<T>} = 116,
@@ -1968,8 +1904,6 @@ construct_runtime!(
 		ChainBridge: chainbridge::{Pallet, Call, Storage, Event<T>} = 151,
 		OrmlAssetRegistry: orml_asset_registry::{Pallet, Storage, Call, Event<T>, Config<T>} = 152,
 		OrmlXcm: orml_xcm::{Pallet, Storage, Call, Event<T>} = 153,
-		PriceOracle: orml_oracle::{Pallet, Call, Storage, Event<T>} = 154,
-		PriceOracleMembership: pallet_membership::{Pallet, Call, Storage, Event<T>} = 155,
 
 		// EVM pallets
 		EVM: pallet_evm::{Pallet, Config, Call, Storage, Event<T>} = 160,
