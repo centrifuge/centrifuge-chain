739321c710dba582583dbff8b3bff4b5b296a9ec
William Freudenberger <w.freude@icloud.com>
2024-02-13 18:07:06 +0100

    chore: bump dev spec version

--- runtime/development/src/lib.rs
+++ runtime/development/src/lib.rs
@@ -142,7 +142,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("centrifuge-devel"),
 	impl_name: create_runtime_str!("centrifuge-devel"),
 	authoring_version: 1,
-	spec_version: 1039,
+	spec_version: 1041,
 	impl_version: 1,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
@@ -2033,7 +2033,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	crate::migrations::UpgradeDevelopment1038,
+	crate::migrations::UpgradeDevelopment1041,
 >;
 
 impl fp_self_contained::SelfContainedCall for RuntimeCall {
