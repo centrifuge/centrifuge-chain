195b14a4351d1057986413fdc10ed37ba35169f5
William Freudenberger <w.freude@icloud.com>
2024-02-20 16:07:48 +0100

    refactor: apply benchmarking v2 to block rewards

--- pallets/block-rewards/src/benchmarking.rs
+++ pallets/block-rewards/src/benchmarking.rs
@@ -1,6 +1,6 @@
 use cfg_primitives::CFG;
 use cfg_types::tokens::CurrencyId;
-use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_benchmarking::v2::*;
 use frame_support::{
 	assert_ok,
 	traits::{fungibles::Inspect, Currency as CurrencyT},
@@ -14,56 +14,85 @@ use crate::{pallet::Config, Pallet as BlockRewards};
 const REWARD: u128 = 1 * CFG;
 const SEED: u32 = 0;
 
-benchmarks! {
-	where_clause {
-		where
+#[benchmarks(
+where
 		T::Balance: From<u128>,
 		T::BlockNumber: From<u32> + One,
 		T::Weight: From<u32>,
 		<T as Config>::Tokens: Inspect<T::AccountId> + CurrencyT<T::AccountId>,
 		<T as Config>::CurrencyId: From<CurrencyId>,
-	}
+)]
+mod benchmarks {
+	use super::*;
 
-	claim_reward {
-		let caller = whitelisted_caller();
-		let beneficiary: T::AccountId =  account("collator", 0, SEED);
+	#[benchmark]
+	fn claim_reward() -> Result<(), BenchmarkError> {
+		let caller: T::AccountId = account("caller", 0, SEED);
+		let beneficiary: T::AccountId = account("collator", 0, SEED);
 
 		assert_ok!(BlockRewards::<T>::do_init_collator(&beneficiary));
-		assert_ok!(T::Rewards::reward_group(T::StakeGroupId::get(), REWARD.into()));
+		assert_ok!(T::Rewards::reward_group(
+			T::StakeGroupId::get(),
+			REWARD.into()
+		));
 		assert!(T::Rewards::is_ready(T::StakeGroupId::get()));
 		assert!(
-			!T::Rewards::compute_reward(
-				T::StakeCurrencyId::get(),
-				&beneficiary,
-			).unwrap().is_zero()
+			!T::Rewards::compute_reward(T::StakeCurrencyId::get(), &beneficiary,)
+				.unwrap()
+				.is_zero()
 		);
-		let before = <T::Tokens as Inspect<T::AccountId>>::balance(CurrencyId::Native.into(), &beneficiary);
+		let before =
+			<T::Tokens as Inspect<T::AccountId>>::balance(CurrencyId::Native.into(), &beneficiary);
 
-	}: _(RawOrigin::Signed(caller), beneficiary.clone())
-	verify {
-		let num_collators: u128 = BlockRewards::<T>::next_session_changes().collator_count.unwrap_or(
-			BlockRewards::<T>::active_session_data().collator_count
-		).into();
-		// Does not get entire reward since another collator is auto-staked via genesis config
-		assert_eq!(<T::Tokens as Inspect<T::AccountId>>::balance(CurrencyId::Native.into(), &beneficiary).saturating_sub(before), (REWARD / (num_collators + 1)).into());
+		#[extrinsic_call]
+		claim_reward(RawOrigin::Signed(caller), beneficiary.clone());
+
+		let num_collators: u128 = BlockRewards::<T>::next_session_changes()
+			.collator_count
+			.unwrap_or(BlockRewards::<T>::active_session_data().collator_count)
+			.into();
+		// Does not get entire reward since another collator is auto-staked via genesis
+		// config
+		assert_eq!(
+			<T::Tokens as Inspect<T::AccountId>>::balance(CurrencyId::Native.into(), &beneficiary)
+				.saturating_sub(before),
+			(REWARD / (num_collators + 1)).into()
+		);
+
+		Ok(())
 	}
 
-	set_collator_reward_per_session {
-	}: _(RawOrigin::Root, REWARD.into())
-	verify {
-		assert_eq!(BlockRewards::<T>::next_session_changes().collator_reward, Some(REWARD.into()));
+	#[benchmark]
+	fn set_collator_reward_per_session() -> Result<(), BenchmarkError> {
+		#[extrinsic_call]
+		set_collator_reward_per_session(RawOrigin::Root, REWARD.into());
+
+		assert_eq!(
+			BlockRewards::<T>::next_session_changes().collator_reward,
+			Some(REWARD.into())
+		);
+
+		Ok(())
 	}
 
-	set_annual_treasury_inflation_rate {
+	#[benchmark]
+	fn set_annual_treasury_inflation_rate() -> Result<(), BenchmarkError> {
 		let rate = T::Rate::saturating_from_rational(1, 2);
-	}: _(RawOrigin::Root, rate)
-	verify {
-		assert_eq!(BlockRewards::<T>::next_session_changes().treasury_inflation_rate, Some(rate));
-	}
-}
 
-impl_benchmark_test_suite!(
-	BlockRewards,
-	crate::mock::ExtBuilder::default().build(),
-	crate::mock::Test,
-);
+		#[extrinsic_call]
+		set_annual_treasury_inflation_rate(RawOrigin::Root, rate);
+
+		assert_eq!(
+			BlockRewards::<T>::next_session_changes().treasury_inflation_rate,
+			Some(rate)
+		);
+
+		Ok(())
+	}
+
+	impl_benchmark_test_suite!(
+		BlockRewards,
+		crate::mock::ExtBuilder::default().build(),
+		crate::mock::Test,
+	);
+}
