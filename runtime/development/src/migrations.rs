23a5c333e910c7c0c3b9dffaa87c04f34512f3c4
William Freudenberger <w.freude@icloud.com>
2024-02-29 10:53:39 +0100

    chore: cleanup Dev migrations

--- runtime/development/src/migrations.rs
+++ runtime/development/src/migrations.rs
@@ -24,19 +24,6 @@ frame_support::parameter_types! {
 }
 
 pub type UpgradeDevelopment1042 = (
-	// Register LocalUSDC
-	runtime_common::migrations::local_currency::register::Migration<
-		super::Runtime,
-		LocalCurrencyIdUsdc,
-	>,
-	// Init local representation for all assets
-	runtime_common::migrations::local_currency::translate_metadata::Migration<
-		super::Runtime,
-		UsdcVariants,
-		LocalAssetIdUsdc,
-	>,
-	// Migrate EpochExecution struct
-	runtime_common::migrations::epoch_execution::Migration<super::Runtime>,
 	// Reset pallets
 	runtime_common::migrations::nuke::ResetPallet<crate::OrderBook, crate::RocksDbWeight, 0>,
 	runtime_common::migrations::nuke::ResetPallet<
@@ -48,5 +35,11 @@ pub type UpgradeDevelopment1042 = (
 	pallet_block_rewards::migrations::v2::RelativeTreasuryInflationMigration<
 		crate::Runtime,
 		AnnualTreasuryInflationPercent,
+	>,
+	// Apply version bump to 1 (storage already reset)
+	runtime_common::migrations::increase_storage_version::Migration<
+		crate::ForeignInvestments,
+		0,
+		1,
 	>,
 );
