4c2eb8433a5c744204a751ad6d4d0ccedef1edd5
William Freudenberger <w.freude@icloud.com>
2024-02-29 11:10:27 +0100

    chore: add no-std feat gate migration tuples

--- runtime/altair/src/migrations.rs
+++ runtime/altair/src/migrations.rs
@@ -28,6 +28,7 @@ frame_support::parameter_types! {
 
 /// The migration set for Altair 1034 @ Kusama. It includes all the migrations
 /// that have to be applied on that chain.
+#[cfg(not(feature = "std"))]
 pub type UpgradeAltair1034 = (
 	// Updates asset custom metadata from mid 2023 to latest (two fields missing/mismatching)
 	translate_asset_metadata::Migration<super::Runtime>,
@@ -89,6 +90,9 @@ pub type UpgradeAltair1034 = (
 	// Data was already moved but storage version not increased from 0 to 4
 	runtime_common::migrations::increase_storage_version::Migration<crate::Council, 0, 4>,
 );
+
+#[cfg(feature = "std")]
+pub type UpgradeAltair1034 = ();
 
 #[allow(clippy::upper_case_acronyms)]
 #[derive(
--- runtime/centrifuge/src/migrations.rs
+++ runtime/centrifuge/src/migrations.rs
@@ -38,6 +38,7 @@ frame_support::parameter_types! {
 	pub const AnnualTreasuryInflationPercent: u32 = 3;
 }
 
+#[cfg(not(feature = "std"))]
 pub type UpgradeCentrifuge1025 = (
 	runtime_common::migrations::epoch_execution::Migration<super::Runtime>,
 	// Migrates the currency used in `pallet-transfer-allowlist` from our global currency to a
@@ -89,17 +90,8 @@ pub type UpgradeCentrifuge1025 = (
 	burn_unburned::Migration<super::Runtime>,
 );
 
-// Copyright 2021 Centrifuge Foundation (centrifuge.io).
-//
-// This file is part of the Centrifuge chain project.
-// Centrifuge is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version (see http://www.gnu.org/licenses).
-// Centrifuge is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+#[cfg(feature = "std")]
+pub type UpgradeCentrifuge1025 = ();
 
 mod burn_unburned {
 	const LOG_PREFIX: &str = "BurnUnburnedMigration: ";
