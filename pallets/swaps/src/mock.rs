f6e06ecdd24fb79ba6c3fb9adb340d5b0bed6444
lemunozm <lemunozm@gmail.com>
2024-03-13 08:26:32 +0100

    migrate pallet-swaps

--- pallets/swaps/src/mock.rs
+++ pallets/swaps/src/mock.rs
@@ -1,15 +1,8 @@
 use cfg_traits::swaps::SwapState;
-use frame_support::traits::{ConstU16, ConstU32, ConstU64};
-use sp_runtime::{
-	testing::{Header, H256},
-	traits::{BlakeTwo256, IdentityLookup},
-	FixedU128,
-};
+use frame_support::derive_impl;
+use sp_runtime::FixedU128;
 
 use crate::pallet as pallet_swaps;
-
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
-type Block = frame_system::mocking::MockBlock<Runtime>;
 
 pub type AccountId = u64;
 pub type Balance = u128;
@@ -18,11 +11,7 @@ pub type SwapId = u32;
 pub type CurrencyId = u8;
 
 frame_support::construct_runtime!(
-	pub enum Runtime where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
+	pub enum Runtime {
 		System: frame_system,
 		MockTokenSwaps: cfg_mocks::token_swaps::pallet,
 		FulfilledSwapHook: cfg_mocks::status_notification::pallet,
@@ -30,31 +19,9 @@ frame_support::construct_runtime!(
 	}
 );
 
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
 impl frame_system::Config for Runtime {
-	type AccountData = ();
-	type AccountId = AccountId;
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockHashCount = ConstU64<250>;
-	type BlockLength = ();
-	type BlockNumber = u64;
-	type BlockWeights = ();
-	type DbWeight = ();
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type Header = Header;
-	type Index = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type MaxConsumers = ConstU32<16>;
-	type OnKilledAccount = ();
-	type OnNewAccount = ();
-	type OnSetCode = ();
-	type PalletInfo = PalletInfo;
-	type RuntimeCall = RuntimeCall;
-	type RuntimeEvent = RuntimeEvent;
-	type RuntimeOrigin = RuntimeOrigin;
-	type SS58Prefix = ConstU16<42>;
-	type SystemWeightInfo = ();
-	type Version = ();
+	type Block = frame_system::mocking::MockBlock<Runtime>;
 }
 
 impl cfg_mocks::token_swaps::pallet::Config for Runtime {
@@ -80,9 +47,5 @@ impl pallet_swaps::Config for Runtime {
 }
 
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	let storage = frame_system::GenesisConfig::default()
-		.build_storage::<Runtime>()
-		.unwrap();
-
-	sp_io::TestExternalities::new(storage)
+	sp_io::TestExternalities::default()
 }
