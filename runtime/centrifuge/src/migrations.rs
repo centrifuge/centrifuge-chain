6b090a8aa7d6586a6efa971377336676468c5907
Frederik Gartenmeister <mustermeiszer@posteo.de>
2024-03-28 13:16:53 +0100

    chore: migration sceleton

--- runtime/centrifuge/src/migrations.rs
+++ runtime/centrifuge/src/migrations.rs
@@ -12,4 +12,31 @@
 
 /// The migration set for Centrifuge @ Polkadot.
 /// It includes all the migrations that have to be applied on that chain.
-pub type UpgradeCentrifuge1027 = ();
+pub type UpgradeCentrifuge1027 = migrate_anemoy_external_prices::Migration<super::Runtime>;
+
+mod migrate_anemoy_external_prices {
+	use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
+
+	const LOG_PREFIX: &str = "MigrateAnemoyPrices:";
+
+	/// Simply bumps the storage version of a pallet
+	///
+	/// NOTE: Use with caution! Must ensure beforehand that a migration is not
+	/// necessary
+	pub struct Migration<R>(sp_std::marker::PhantomData<R>);
+	impl<R> OnRuntimeUpgrade for Migration<R> {
+		fn on_runtime_upgrade() -> Weight {
+			Weight::zero()
+		}
+
+		#[cfg(feature = "try-runtime")]
+		fn pre_upgrade() -> Result<sp_std::vec::Vec<u8>, sp_runtime::DispatchError> {
+			Ok(sp_std::vec![])
+		}
+
+		#[cfg(feature = "try-runtime")]
+		fn post_upgrade(_: sp_std::vec::Vec<u8>) -> Result<(), sp_runtime::DispatchError> {
+			Ok(())
+		}
+	}
+}
