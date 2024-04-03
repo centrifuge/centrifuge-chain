64585d6c4163b109eb72df994267281d1a46dd1d
Frederik Gartenmeister <mustermeiszer@posteo.de>
2024-04-03 09:30:31 +0200

    feat: update nav always working for runtime api

--- runtime/centrifuge/src/lib.rs
+++ runtime/centrifuge/src/lib.rs
@@ -2345,7 +2345,7 @@ impl_runtime_apis! {
 		fn tranche_token_price(pool_id: PoolId, tranche: TrancheLoc<TrancheId>) -> Option<Quantity>{
 			let now = <Timestamp as UnixTime>::now().as_secs();
 			let mut pool = PoolSystem::pool(pool_id)?;
-			let nav = Loans::update_nav(pool_id).ok()?;
+			let nav = runtime_common::apis::loans::update_nav_api_call(pool_id).ok()?;
 			let total_assets = pool.reserve.total.saturating_add(nav);
 			let index: usize = pool.tranches.tranche_index(&tranche)?.try_into().ok()?;
 			let prices = pool
@@ -2358,7 +2358,7 @@ impl_runtime_apis! {
 		fn tranche_token_prices(pool_id: PoolId) -> Option<Vec<Quantity>>{
 			let now = <Timestamp as UnixTime>::now().as_secs();
 			let mut pool = PoolSystem::pool(pool_id)?;
-			let nav = Loans::update_nav(pool_id).ok()?;
+			let nav = runtime_common::apis::loans::update_nav_api_call(pool_id).ok()?;
 			let total_assets = pool.reserve.total.saturating_add(nav);
 			pool
 				.tranches
@@ -2384,7 +2384,7 @@ impl_runtime_apis! {
 
 		fn nav(pool_id: PoolId) -> Option<PoolNav<Balance>> {
 			let pool = pallet_pool_system::Pool::<Runtime>::get(pool_id)?;
-			let nav_loans = Loans::update_nav(pool_id).ok()?;
+			let nav_loans = runtime_common::apis::loans::update_nav_api_call(pool_id).ok()?;
 			let nav_fees = PoolFees::update_nav(pool_id).ok()?;
 			let nav = pallet_pool_system::Nav::new(nav_loans, nav_fees);
 			let total = nav.total(pool.reserve.total).unwrap_or(Balance::default());
