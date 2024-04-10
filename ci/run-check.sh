43e5b4d406132a8375dd9ed4d327ae1215595a24
William Freudenberger <w.freude@icloud.com>
2024-04-10 16:19:43 +0200

    wip 3: use du instead of ll debug

--- ci/run-check.sh
+++ ci/run-check.sh
@@ -19,12 +19,11 @@ case $TARGET in
 
   test-integration)
     # TODO: Remove before merging PR
-    ls target/debug/build/runtime-integration-tests-*/out
-    ls target/debug/build/runtime-integration-tests-*/out/liquidity-pools
+    du -h target/debug/build/runtime-integration-tests-*
     rm -rf target/debug/deps/runtime_integration_tests-*
     rm -rf target/debug/build/runtime-integration*
     git submodule update
-    ll runtime/integration-tests/submodules/liquidity-pools
+    du -h runtime/integration-tests/submodules/liquidity-pools
     cargo test --release --package runtime-integration-tests --features fast-runtime
     ;;
 
