c6e218128c784304bbdc55f99004aad4e9b86c77
William Freudenberger <w.freude@icloud.com>
2024-04-11 10:11:52 +0200

    wip 6

--- ci/run-check.sh
+++ ci/run-check.sh
@@ -23,10 +23,10 @@ case $TARGET in
     rm -rf target/debug/deps/runtime_integration_tests-*
     rm -rf target/debug/build/runtime-integration*
     
-    git submodule status
-    git submodule update --init --recursive --remote
+    # git submodule status
+    # git submodule update --init --recursive --remote
     find runtime/integration-tests/submodules/ -name "liquidity-pools" -print0 | xargs -0 -r du -h
-    git submodule status
+    # git submodule status
 
     cargo test --release --package runtime-integration-tests --features fast-runtime
     ;;
