944787f0c3a368161cd92f644160c78a974641e3
William Freudenberger <w.freude@icloud.com>
2024-04-30 11:37:13 +0200

    fix verbose logging

--- ci/run-check.sh
+++ ci/run-check.sh
@@ -18,7 +18,7 @@ case $TARGET in
 #    ;;
 
   test-integration)
-    cargo build --release --package runtime-integration-tests --vv
+    cargo build --release --package runtime-integration-tests -vv
 
     echo "NOW TESTING"
     cargo test --release --package runtime-integration-tests --features fast-runtime &
