f6d56b0ef027f8afa22bc08d0253a6334306a992
lemunozm <lemunozm@gmail.com>
2024-05-10 12:48:16 +0200

    remove crowdloan pallets

--- libs/traits/src/lib.rs
+++ libs/traits/src/lib.rs
@@ -61,63 +61,6 @@ pub mod swaps;
 /// Traits related to benchmarking tooling.
 pub mod benchmarking;
 
-/// A trait used for loosely coupling the claim pallet with a reward mechanism.
-///
-/// ## Overview
-/// The crowdloan reward mechanism is separated from the crowdloan claiming
-/// process, the latter being generic, acting as a kind of proxy to the
-/// rewarding mechanism, that is specific to to each crowdloan campaign. The aim
-/// of this pallet is to ensure that a claim for a reward payout is well-formed,
-/// checking for replay attacks, spams or invalid claim (e.g. unknown
-/// contributor, exceeding reward amount, ...).
-/// See the [`crowdloan-reward`] pallet, that implements a reward mechanism with
-/// vesting, for instance.
-pub trait Reward {
-	/// The account from the parachain, that the claimer provided in her/his
-	/// transaction.
-	type ParachainAccountId: Debug
-		+ MaybeSerialize
-		+ MaybeSerializeDeserialize
-		+ Member
-		+ Ord
-		+ Parameter
-		+ TypeInfo;
-
-	/// The contribution amount in relay chain tokens.
-	type ContributionAmount: AtLeast32BitUnsigned
-		+ Codec
-		+ Copy
-		+ Debug
-		+ Default
-		+ MaybeSerializeDeserialize
-		+ Member
-		+ Parameter
-		+ Zero
-		+ TypeInfo;
-
-	/// Block number type used by the runtime
-	type BlockNumber: AtLeast32BitUnsigned
-		+ Bounded
-		+ Copy
-		+ Debug
-		+ Default
-		+ Hash
-		+ MaybeDisplay
-		+ MaybeSerializeDeserialize
-		+ Member
-		+ Parameter
-		+ TypeInfo;
-
-	/// Rewarding function that is invoked from the claim pallet.
-	///
-	/// If this function returns successfully, any subsequent claim of the same
-	/// claimer will be rejected by the claim module.
-	fn reward(
-		who: Self::ParachainAccountId,
-		contribution: Self::ContributionAmount,
-	) -> DispatchResultWithPostInfo;
-}
-
 /// A trait that can be used to fetch the nav and update nav for a given pool
 pub trait PoolNAV<PoolId, Amount> {
 	type ClassId;
