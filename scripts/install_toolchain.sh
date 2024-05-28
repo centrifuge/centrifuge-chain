8071ffef900c00b180ab8323f8cbb9b523d60872
lemunozm <lemunozm@gmail.com>
2024-05-28 09:19:30 +0200

    allow nightly support in CI

--- scripts/install_toolchain.sh
+++ scripts/install_toolchain.sh
@@ -11,7 +11,9 @@ echo "*** Initializing WASM build environment"
 rustup update $RUST_TOOLCHAIN
 
 rustup toolchain install $RUST_TOOLCHAIN
+rustup toolchain install nightly
 rustup default $RUST_TOOLCHAIN
 rustup component add rustfmt
+rustup component add --toolchain nightly rustfmt
 
 rustup target add wasm32-unknown-unknown --toolchain $RUST_TOOLCHAIN
