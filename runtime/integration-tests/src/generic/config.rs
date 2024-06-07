4be5b03041e1a2b32db973f3b912a88166e9628c
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2024-06-07 08:14:05 +0200

    fix it (#1866)

--- runtime/integration-tests/src/generic/config.rs
+++ runtime/integration-tests/src/generic/config.rs
@@ -282,6 +282,7 @@ pub trait Runtime:
 				frame_system::CheckWeight<Self>,
 				pallet_transaction_payment::ChargeTransactionPayment<Self>,
 				runtime_common::transfer_filter::PreBalanceTransferExtension<Self>,
+				frame_metadata_hash_extension::CheckMetadataHash<Self>,
 			),
 		>,
 	>;
--- runtime/integration-tests/src/generic/env.rs
+++ runtime/integration-tests/src/generic/env.rs
@@ -187,6 +187,7 @@ pub mod utils {
 			frame_system::CheckWeight::<T>::new(),
 			pallet_transaction_payment::ChargeTransactionPayment::<T>::from(0),
 			runtime_common::transfer_filter::PreBalanceTransferExtension::<T>::new(),
+			frame_metadata_hash_extension::CheckMetadataHash::<T>::new(true),
 		);
 
 		let raw_payload = SignedPayload::new(runtime_call.clone(), signed_extra.clone()).unwrap();
