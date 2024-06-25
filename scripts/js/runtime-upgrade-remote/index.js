30086c8a453c65b7be00dca534abf394dfaf7d4c
William Freudenberger <w.freude@icloud.com>
2024-06-25 11:37:53 +0200

    feat: improve error catching

--- scripts/js/runtime-upgrade-remote/index.js
+++ scripts/js/runtime-upgrade-remote/index.js
@@ -1,6 +1,6 @@
 const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
 const { u8aToHex } = require('@polkadot/util');
-const { blake2AsHex } = require('@polkadot/util-crypto');
+const { blake2AsHex, blake2AsU8a } = require('@polkadot/util-crypto');
 const fs = require('fs');
 const path = require('path');
 
@@ -13,7 +13,7 @@ const run = async () => {
   try {
     // Validate configuration
     if (!config.endpoint || !config.wasmFile || !config.privateKey) {
-      console.error("Missing configuration parameters. Please ensure 'endpoint', 'wasmFile', and 'privateKey' are specified in config.json.");
+      console.error("Missing configuration parameters. Please ensure 'endpoint', 'wasmFile', and 'privateKey' are specified in the corresponding configs/*.json.");
       process.exit(1);
     }
 
@@ -35,18 +35,17 @@ const run = async () => {
     console.log(`Using account: ${user.address}`);
 
     const wasm = fs.readFileSync(config.wasmFile);
-    const wasmHex = `0x${wasm.toString('hex')}`;
+    const wasmHash = blake2AsHex(wasm);
     const wasmBytes = u8aToHex(wasm);
 
     console.log("WASM file loaded and ready for deployment");
 
     if (config.sudo) {
       console.log("Using sudo to perform the runtime upgrade");
-      await sudoAuthorize(api, user, wasmHex);
+      await sudoAuthorize(api, user, wasmHash);
       await enactUpgrade(api, user, wasmBytes);
     } else {
-      console.log("Using council proposal to perform the runtime upgrade");
-      await councilUpgrade(config, api, user)
+      console.error("Unsupported");
     }
     // Check for specific events or transaction success as needed
   } catch (error) {
@@ -58,30 +57,23 @@ const run = async () => {
 };
 
 async function sudoAuthorize(api, sudoAccount, wasmHex) {
-  const nonce = await getNonce(api, user.address);
+  const nonce = await api.rpc.system.accountNextIndex(sudoAccount.address)
 
   return new Promise(async (resolve, reject) => {
     try {
-      // Hash the WASM blob
-      const wasmHash = blake2AsHex(wasmHex);
-      console.log(wasmHash);
-
       // Authorize the upgrade
       const authorizeTx = api.tx.sudo.sudo(
-        api.tx.parachainSystem.authorizeUpgrade(wasmHash, true)
+        api.tx.parachainSystem.authorizeUpgrade(wasmHex, true)
       );
 
-      const unsub = await authorizeTx.signAndSend(sudoAccount, { nonce }, ({ status, dispatchError }) => {
+      const unsub = await authorizeTx.signAndSend(sudoAccount, { nonce }, ({ status, dispatchError, events }) => {
         console.log(`Authorizing upgrade with status ${status}`);
         if (status.isInBlock) {
           console.log(`Authorization included in block ${status.asInBlock}`);
           resolve();
           unsub();
         }
-        if (dispatchError) {
-          console.error(`Error: ${dispatchError}`);
-          reject(dispatchError);
-        }
+        checkError(api, reject, dispatchError, events)
       });
     }
     catch (error) {
@@ -91,30 +83,66 @@ async function sudoAuthorize(api, sudoAccount, wasmHex) {
 }
 
 async function enactUpgrade(api, sudoAccount, wasmFile) {
-  const nonce = await getNonce(api, user.address);
+  const nonce = await api.rpc.system.accountNextIndex(sudoAccount.address)
 
   return new Promise(async (resolve, reject) => {
     try {
       // Enact the authorized upgrade
       const enactTx = api.tx.parachainSystem.enactAuthorizedUpgrade(wasmFile);
 
-      const unsub = await enactTx.signAndSend(sudoAccount, { nonce }, ({ status, dispatchError }) => {
+      const unsub = await enactTx.signAndSend(sudoAccount, { nonce }, ({ status, dispatchError, events }) => {
         console.log(`Enacting upgrade with status ${status}`);
         if (status.isInBlock) {
           console.log(`Enactment included in block ${status}`);
           resolve();
           unsub();
         }
-        if (dispatchError) {
-          console.error(`Error: ${dispatchError}`);
-          reject(dispatchError);
-        }
+        checkError(api, reject, dispatchError, events)
       });
     }
     catch (error) {
       reject(error)
     }
   });
+}
+
+function checkError(api, reject, dispatchError, events) {
+  if (dispatchError) {
+    if (dispatchError.isModule) {
+      // for module errors, we have the section indexed, lookup
+      const decoded = api.registry.findMetaError(dispatchError.asModule);
+      const { docs, name, section } = decoded;
+
+      console.error(`${section}.${name}: ${docs.join(' ')}`);
+    } else {
+      // Other, CannotLookup, BadOrigin, no extra info
+      console.error(dispatchError.toString());
+    }
+    reject(dispatchError)
+  } else if (events) {
+    events
+      // find/filter for failed events
+      .filter(({ event }) =>
+        api.events.system.ExtrinsicFailed.is(event)
+      )
+      // we know that data for system.ExtrinsicFailed is
+      // (DispatchError, DispatchInfo)
+      .forEach(({ event: { data: [error, info] } }) => {
+        if (error.isModule) {
+          // for module errors, we have the section indexed, lookup
+          const decoded = api.registry.findMetaError(error.asModule);
+          const { docs, method, section } = decoded;
+          const error = `${section}.${method}: ${docs.join(' ')}`
+
+          console.error(error);
+          reject(error)
+        } else {
+          // Other, CannotLookup, BadOrigin, no extra info
+          console.error(error.toString());
+          reject(error.toString())
+        }
+      });
+  }
 }
 
 run();
