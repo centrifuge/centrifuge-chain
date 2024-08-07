61aeae72f218c3f675cc01daa3879a6b014f633a
William Freudenberger <w.freude@icloud.com>
2024-08-07 12:26:45 +0200

    fix: gateway migration

--- pallets/liquidity-pools-gateway/src/lib.rs
+++ pallets/liquidity-pools-gateway/src/lib.rs
@@ -86,7 +86,10 @@ pub mod pallet {
 		MalformedSourceChain, MalformedSourceChainLength,
 	};
 
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
 	#[pallet::pallet]
+	#[pallet::storage_version(STORAGE_VERSION)]
 	pub struct Pallet<T>(_);
 
 	#[pallet::origin]
