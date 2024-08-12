657f9d24cd9de3b179c0dc2f91f7c17f2e2dc0c5
lemunozm <lemunozm@gmail.com>
2024-08-12 19:20:54 +0200

    add account code checker

--- runtime/common/src/routing.rs
+++ runtime/common/src/routing.rs
@@ -1,8 +1,15 @@
-use cfg_primitives::AccountId;
-use cfg_traits::liquidity_pools::{MessageSender, RouterSupport};
+use cfg_traits::{
+	liquidity_pools::{MessageSender, RouterSupport},
+	PreConditions,
+};
 use cfg_types::domain_address::{Domain, DomainAddress};
-use frame_support::dispatch::DispatchResultWithPostInfo;
+use frame_support::{
+	dispatch::DispatchResultWithPostInfo,
+	pallet_prelude::{Decode, Encode, MaxEncodedLen, TypeInfo},
+};
 use pallet_axelar_router::AxelarId;
+use sp_core::{H160, H256};
+use sp_runtime::traits::{BlakeTwo256, Hash};
 use sp_std::marker::PhantomData;
 
 /// Identification of the router where the message is sent and received
@@ -33,7 +40,6 @@ impl RouterSupport<Domain> for RouterId {
 
 /// This type choose the correct router implementation given a router id
 struct RouterDispatcher<Routers>(PhantomData<Routers>);
-
 impl<Routers> MessageSender for RouterDispatcher<Routers>
 where
 	Routers: pallet_axelar_router::Config,
@@ -51,5 +57,15 @@ where
 				pallet_axelar_router::Pallet::<Routers>::send(axelar_id, origin, message)
 			}
 		}
+	}
+}
+
+struct EvmAccountCodeChecker<Runtime>(PhantomData<Runtime>);
+impl<Runtime: pallet_evm::Config> PreConditions<(H160, H256)> for EvmAccountCodeChecker<Runtime> {
+	type Result = bool;
+
+	fn check((contract_address, contract_hash): (H160, H256)) -> bool {
+		let code = pallet_evm::AccountCodes::<Runtime>::get(contract_address);
+		BlakeTwo256::hash_of(&code) == contract_hash
 	}
 }
