cc8e3e4c57ab3f1e3a9531d899dcee8b600af179
Frederik Gartenmeister <mustermeiszer@posteo.de>
2025-01-16 16:57:31 +0100

    pallet v2

--- pallets/cfg-migration/src/lib.rs
+++ pallets/cfg-migration/src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_support::{
-    dispatch::DispatchResult, pallet_prelude::*, traits::Currency,
+    dispatch::DispatchResult, pallet_prelude::*,
 };
 
 #[cfg(test)]
@@ -27,12 +27,8 @@ pub mod pallet {
     use frame_support::PalletId;
     use frame_support::sp_runtime::traits::AccountIdConversion;
     use frame_system::pallet_prelude::*;
-
-    type BalanceOf<T> =
-    <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
-
-    type CurrencyIdFor<T> = <T as pallet_liquidity_pools::Config>::CurrencyId;
-
+    use frame_support::traits::fungibles::Mutate;
+    use frame_support::traits::tokens::Preservation;
 
     #[pallet::pallet]
     pub struct Pallet<T>(_);
@@ -40,21 +36,19 @@ pub mod pallet {
     #[pallet::config]
     pub trait Config: frame_system::Config + pallet_liquidity_pools::Config {
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-
-        type Currency: Currency<Self::AccountId, Balance = Self::Balance>;
-        type PalletAccount: Get<PalletId>;
-        type IouCfg: Get<CurrencyIdFor<Self>>;
+        type CfgLockAccount: Get<PalletId>;
+        type IouCfg: Get<Self::CurrencyId>;
+        type NativeCfg: Get<Self::CurrencyId>;
         type EVMChainId: Get<u64>;
         type WeightInfo: WeightInfo;
     }
-
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         CfgMigrationInitiated {
             sender: T::AccountId,
             receiver: H160,
-            amount: BalanceOf<T>,
+            amount: T::Balance,
         },
     }
 
@@ -64,18 +58,18 @@ pub mod pallet {
         #[pallet::call_index(0)]
         pub fn migrate(
             origin: OriginFor<T>,
-            cfg_amount: BalanceOf<T>,
+            cfg_amount: T::Balance,
             receiver: H160,
         ) -> DispatchResult {
             let who = ensure_signed(origin)?;
 
-            let pallet = T::PalletAccount::get().into_account_truncating();
-            T::Currency::transfer(&who, &pallet, cfg_amount, frame_support::traits::ExistenceRequirement::AllowDeath)?;
+            let cfg_lock_account = T::CfgLockAccount::get().into_account_truncating();
+            let iou = T::IouCfg::get();
 
-            let domain_address = DomainAddress::Evm(T::EVMChainId::get(), receiver);
-            let origin = OriginFor::<T>::signed(pallet);
+            T::Tokens::transfer(T::NativeCfg::get(), &who, &cfg_lock_account, cfg_amount, Preservation::Expendable)?;
+            T::Tokens::mint_into(iou, &cfg_lock_account , cfg_amount)?;
 
-            pallet_liquidity_pools::Pallet::<T>::transfer(origin, T::IouCfg::get(), domain_address, cfg_amount)?;
+            pallet_liquidity_pools::Pallet::<T>::transfer(OriginFor::<T>::signed(cfg_lock_account), T::IouCfg::get(), DomainAddress::Evm(T::EVMChainId::get(), receiver), cfg_amount)?;
 
             Self::deposit_event(Event::CfgMigrationInitiated{
                 sender: who,
